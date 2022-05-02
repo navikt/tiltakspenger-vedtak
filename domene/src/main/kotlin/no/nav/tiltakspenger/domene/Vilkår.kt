6bf9973b8aea6b7dda63104c30e0e3c959981485
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-05-02 13:01:08 +0200

    Do not automatically deploy to NAIS dev as this repo is very much work in progress

--- domene/src/main/kotlin/no/nav/tiltakspenger/domene/Vilkår.kt
+++ domene/src/main/kotlin/no/nav/tiltakspenger/domene/Vilkår.kt
@@ -1,5 +1,23 @@
 package no.nav.tiltakspenger.domene
 
-fun main() {
-    println("Hei")
+enum class Paragraf(beskrivelse: String) {
+    PARAGRAF_3_LEDD_1_PUNKTUM1("Tiltakspenger kan gis til tiltaksdeltakere som har fylt 18 år"),
+    PARAGRAF_3_LEDD_1_PUNKTUM3("Det gis et barnetillegg for hvert barn under 16 år som tiltaksdeltakeren forsørger."),
+    PARAGRAF_7_LEDD_1_PUNKTUM1("")
+}
+
+interface Vilkår {
+    val paragraf: Paragraf?
+    fun sjekk(faktum: List<Faktum>): Boolean
+}
+
+object ErOver18År : Vilkår {
+    override val paragraf = Paragraf.PARAGRAF_3_LEDD_1_PUNKTUM1
+    override fun sjekk(faktum: List<Faktum>): Boolean {
+        when (faktum.first() is AldersFaktum) {
+            true -> (faktum.first() as AldersFaktum).erOver18()
+            else -> throw IllegalArgumentException("Wrong faktum")
+        }
+        return false
+    }
 }
