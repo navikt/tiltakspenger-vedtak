b0f18d15ad6a12c6721e51c2522c0310c5b22f8c
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-09-05 19:32:33 +0200

    Add a property for jackson-version to avoid getting multiple pull requests from dependabot

--- domene/build.gradle.kts
+++ domene/build.gradle.kts
@@ -1,13 +1,14 @@
+val jacksonVersion = "2.13.4"
+
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
     implementation("com.google.guava:guava:31.1-jre")
-
-    implementation("com.fasterxml.jackson.core:jackson-core:2.13.4")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.3")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:$jacksonVersion")
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
 
     testImplementation(kotlin("test"))
 }
