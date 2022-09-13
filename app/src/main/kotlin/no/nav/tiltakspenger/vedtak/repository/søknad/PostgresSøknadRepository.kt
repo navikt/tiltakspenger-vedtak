b09e5237de7214e09eef66a603b1ee98d86ea8e2
Richard Martinsen <github@martinsen.org>
2022-09-13 15:08:00 +0200

    wip utvide tabellene og Repo

--- app/src/main/kotlin/no/nav/tiltakspenger/vedtak/repository/søknad/PostgresSøknadRepository.kt
+++ app/src/main/kotlin/no/nav/tiltakspenger/vedtak/repository/søknad/PostgresSøknadRepository.kt
@@ -2,41 +2,308 @@ package no.nav.tiltakspenger.vedtak.repository.søknad
 
 import kotliquery.Row
 import kotliquery.queryOf
-import no.nav.tiltakspenger.vedtak.Barnetillegg
+import no.nav.tiltakspenger.vedtak.ArenaTiltak
+import no.nav.tiltakspenger.vedtak.BrukerregistrertTiltak
 import no.nav.tiltakspenger.vedtak.Søknad
+import no.nav.tiltakspenger.vedtak.TrygdOgPensjon
 import no.nav.tiltakspenger.vedtak.db.DataSource.session
 import org.intellij.lang.annotations.Language
+import java.util.*
 
-internal class PostgresSøknadRepository: SøknadRepository {
+internal class PostgresSøknadRepository : SøknadRepository {
 
-    @Language("SQL")
-    private val lagre = "insert into søknad (id, ident, tilstand) values (:id, :ident, :tilstand)"
-
-    @Language("SQL")
-    private val slett = "delete from søknad where id = ?"
+    private val barnetilleggRepo = BarnetilleggRepo()
 
     @Language("SQL")
-    private val finnes = "select exists(select 1 from søknad where ident = ?)"
+    private val lagreSøknad = """
+        insert into søknad (
+            id, 
+            fornavn, 
+            etternavn, 
+            ident, 
+            deltar_kvp, 
+            deltar_intro, 
+            institusjon_opphold, 
+            institusjon_type,
+            fritekst,
+            journalpost_id,
+            dokumentinfo_id,
+            tidsstempel_kilde,
+            tidsstempel_hos_oss
+        ) values (
+            :id, 
+            :fornavn, 
+            :etternavn,
+            :ident,
+            :deltarKvp,
+            :deltarIntro,
+            :instOpphold,
+            :instType,
+            :fritekst,
+            :journalpostId,
+            :dokumentinfoId,
+            :tidsstempelKilde,
+            :tidsstempelHosOss
+        )""".trimIndent()
+
+    @Language("SQL")
+    private val oppdaterSøknad = """
+        update søknad set  
+            fornavn = :fornavn, 
+            etternavn = :etternavn, 
+            ident = :ident, 
+            deltar_kvp = :deltarKvp, 
+            deltar_intro = :deltarIntro, 
+            institusjon_opphold = :instOpphold, 
+            institusjon_type = :instType,
+            fritekst = :fritekst,
+            journalpost_id = :journalpostId,
+            dokumentinfo_id = :dokumentinfoId,
+            tidsstempel_kilde = :tidsstempelKilde,
+            tidsstempel_hos_oss = :tidsstempelHosOss
+        where id = :id
+        )""".trimIndent()
+
+
+    @Language("SQL")
+    private val lagreArenaTiltak = """
+        insert into arenatiltak (
+            id,
+            søknad_id,
+            arena_id,
+            arrangoer, 
+            har_sluttdato_fra_arena, 
+            navn,
+            er_i_endre_status,
+            opprinnelig_startdato,
+            opprinnelig_sluttdato,
+            startdato,
+            sluttdato
+        ) values (
+            :id,
+            :soknadId,
+            :arenaId,
+            :arrangoer, 
+            :harSluttdatoFraArena,
+            :navn,
+            :erIEndreStatus,
+            :opprinneligStartdato,
+            :opprinneligSluttdato,
+            :startdato,
+            :sluttdato
+        )""".trimIndent()
+
+    @Language("SQL")
+    private val lagreBrukerTiltak = """
+        insert into brukertiltak (
+            id,
+            søknad_id,
+            tiltakstype,
+            arrangoernavn, 
+            beskrivelse, 
+            fom,
+            tom,
+            adresse,
+            postnummer,
+            antall_dager
+        ) values (
+            :id,
+            :soknadId,
+            :tiltakstype,
+            :arrangoernavn, 
+            :beskrivelse,
+            :fom,
+            :tom,
+            :adresse,
+            :postnummer,
+            :antallDager
+        )""".trimIndent()
+
+    @Language("SQL")
+    private val lagreTrygdOgPensjon = """
+        insert into trygdogpensjon (
+            id,
+            søknad_id,
+            utbetaler,
+            prosent,
+            fom,
+            tom
+        ) values (
+            :id,
+            :soknadId,
+            :utbetaler,
+            :prosent,
+            :fom,
+            :tom
+        )""".trimIndent()
+
+
+    @Language("SQL")
+    private val slettArenaTiltak = "delete from arenatiltak where søknad_id = ?"
+
+    @Language("SQL")
+    private val slettBrukerregistrertTiltak = "delete from brukertiltak where søknad_id = ?"
+
+    @Language("SQL")
+    private val slettTrygdOgPensjon = "delete from trygdogpensjon where søknad_id = ?"
+
+    @Language("SQL")
+    private val finnes = "select exists(select 1 from søknad where id = ?)"
 
     @Language("SQL")
     private val hentAlle = "select * from søknad where ident = ?"
 
     override fun hentAlle(ident: String): List<Søknad> {
-        return session.run(queryOf(hentAlle, ident)
-            .map { row ->
-                row.toSøknad()
-            }.asList)
+        return session.run(
+            queryOf(hentAlle, ident)
+                .map { row ->
+                    row.toSøknad()
+                }.asList
+        )
     }
 
-    private fun slettAlle(ident: String): Unit {
-        // TODO: later når vi har skrevet noen tester
-//        session.run(
-//
-//        )
+    private fun slettArenatiltak(søknadId: UUID): Unit {
+        session.run(
+            queryOf(slettArenaTiltak, søknadId).asUpdate
+        )
+    }
+
+    private fun slettBrukertiltak(søknadId: UUID): Unit {
+        session.run(
+            queryOf(slettBrukerregistrertTiltak, søknadId).asUpdate
+        )
+    }
+
+    private fun slettTrygdOgPensjon(søknadId: UUID): Unit {
+        session.run(
+            queryOf(slettTrygdOgPensjon, søknadId).asUpdate
+        )
+    }
+
+    private fun søknadFinnes(søknadId: UUID): Boolean = session.run(
+        queryOf(finnes, søknadId).map { row -> row.boolean("exists") }.asSingle
+    ) ?: throw InternalError("Failed to check if person exists")
+
+    private fun lagreHeleSøknaden(søknad: Søknad) {
+        barnetilleggRepo.lagre(søknad.id, søknad.barnetillegg)
+
+        slettArenatiltak(søknad.id)
+        if (søknad.arenaTiltak != null) lagreArenaTiltak(søknad.id, søknad.arenaTiltak!!)
+        slettBrukertiltak(søknad.id)
+        if (søknad.brukerregistrertTiltak != null) lagreBrukerTiltak(søknad.id, søknad.brukerregistrertTiltak!!)
+        slettTrygdOgPensjon(søknad.id)
+        søknad.trygdOgPensjon?.forEach { lagreTrygdOgPensjon(søknad.id, it) }
+
+        if (søknadFinnes(søknad.id)) {
+            oppdaterSøknad(søknad)
+        } else {
+            lagreSøknad(søknad)
+        }
+    }
+
+    private fun oppdaterSøknad(søknad: Søknad) {
+        session.run(
+            queryOf(
+                oppdaterSøknad, mapOf(
+                    "id" to søknad.id,
+                    "fornavn" to søknad.fornavn,
+                    "etternavn" to søknad.etternavn,
+                    "ident" to søknad.ident,
+                    "deltarKvp" to søknad.deltarKvp,
+                    "deltarIntro" to søknad.deltarIntroduksjonsprogrammet,
+                    "instOpphold" to søknad.oppholdInstitusjon,
+                    "instType" to søknad.typeInstitusjon,
+                    "fritekst" to søknad.fritekst,
+                    "journalpostId" to søknad.journalpostId,
+                    "dokumentinfoId" to søknad.dokumentInfoId,
+                    "tidsstempelKilde" to søknad.opprettet,
+                    "tidsstempelHosOss" to søknad.innhentet,
+                )
+            ).asUpdate
+        )
+    }
+
+    private fun lagreSøknad(søknad: Søknad) {
+        session.run(
+            queryOf(
+                lagreSøknad, mapOf(
+                    "id" to søknad.id,
+                    "fornavn" to søknad.fornavn,
+                    "etternavn" to søknad.etternavn,
+                    "ident" to søknad.ident,
+                    "deltarKvp" to søknad.deltarKvp,
+                    "deltarIntro" to søknad.deltarIntroduksjonsprogrammet,
+                    "instOpphold" to søknad.oppholdInstitusjon,
+                    "instType" to søknad.typeInstitusjon,
+                    "fritekst" to søknad.fritekst,
+                    "journalpostId" to søknad.journalpostId,
+                    "dokumentinfoId" to søknad.dokumentInfoId,
+                    "tidsstempelKilde" to søknad.opprettet,
+                    "tidsstempelHosOss" to søknad.innhentet,
+                )
+            ).asUpdate
+        )
+    }
+
+
+    private fun lagreArenaTiltak(søknadId: UUID, arenaTiltak: ArenaTiltak) {
+        session.run(
+            queryOf(
+                lagreArenaTiltak, mapOf(
+                    "id" to UUID.randomUUID(),
+                    "soknadId" to søknadId,
+                    "arenaId" to arenaTiltak.arenaId,
+                    "arrangoer" to arenaTiltak.arrangoer,
+                    "harSluttdatoFraArena" to arenaTiltak.harSluttdatoFraArena,
+                    "navn" to arenaTiltak.tiltakskode,                  // TODO sjekk om denne er riktig, og om den skal endre navn i basen
+                    "erIEndreStatus" to arenaTiltak.erIEndreStatus,
+                    "opprinneligStartdato" to arenaTiltak.opprinneligStartdato,
+                    "opprinneligSluttdato" to arenaTiltak.opprinneligSluttdato,
+                    "startdato" to arenaTiltak.startdato,
+                    "sluttdato" to arenaTiltak.sluttdato,
+                )
+            ).asUpdate
+        )
+    }
+
+    private fun lagreBrukerTiltak(søknadId: UUID, brukerregistrertTiltak: BrukerregistrertTiltak) {
+        session.run(
+            queryOf(
+                lagreBrukerTiltak, mapOf(
+                    "id" to UUID.randomUUID(),
+                    "soknadId" to søknadId,
+                    "tiltakstype" to brukerregistrertTiltak.tiltakskode,    // TODO skal denne endre navn i basen ?
+                    "arrangoernavn" to brukerregistrertTiltak.arrangoernavn,
+                    "beskrivelse" to brukerregistrertTiltak.beskrivelse,
+                    "fom" to brukerregistrertTiltak.fom,
+                    "tom" to brukerregistrertTiltak.tom,
+                    "adresse" to brukerregistrertTiltak.adresse,
+                    "postnummer" to brukerregistrertTiltak.postnummer,
+                    "antallDager" to brukerregistrertTiltak.antallDager,
+                )
+            ).asUpdate
+        )
+    }
+
+    private fun lagreTrygdOgPensjon(søknadId: UUID, trygdOgPensjon: TrygdOgPensjon) {
+        session.run(
+            queryOf(
+                lagreTrygdOgPensjon, mapOf(
+                    "id" to UUID.randomUUID(),
+                    "soknadId" to søknadId,
+                    "utbetaler" to trygdOgPensjon.utbetaler,
+                    "prosent" to trygdOgPensjon.prosent,
+                    "fom" to trygdOgPensjon.fom,
+                    "tom" to trygdOgPensjon.tom,
+                )
+            ).asUpdate
+        )
     }
 
     override fun lagre(ident: String, søknader: List<Søknad>): Int {
-        slettAlle(ident)
+        søknader.forEach {
+            lagreHeleSøknaden(it)
+        }
         return 0
     }
 
@@ -56,11 +323,12 @@ internal class PostgresSøknadRepository: SøknadRepository {
         val brukerRegistrertSluttDato = localDate("bruker_reg_sluttdato")
         val systemRegistrertStartDato = localDate("system_reg_startdato")
         val systemRegistrertSluttDato = localDate("system_reg_sluttdato")
-        val barnetillegg = emptyList<Barnetillegg>() // TODO kalle barnetillegg.hentAlle(ident)
         val innhentet = localDateTime("innhentet")
+        val barnetillegg = barnetilleggRepo.hentBarnetilleggListe(id)
 
         return Søknad(
-            id = id,,
+            id = id,
+            søknadId = "",
             fornavn = fornavn,
             etternavn = etternavn,
             ident = ident,
@@ -73,8 +341,10 @@ internal class PostgresSøknadRepository: SøknadRepository {
             innhentet = innhentet,
             arenaTiltak = null,
             brukerregistrertTiltak = null,
-            trygdOgPensjon = null,,
-            fritekst = null
+            trygdOgPensjon = null,
+            fritekst = null,
+            dokumentInfoId = "",
+            journalpostId = "",
         )
     }
 }
--- app/src/main/resources/db/migration/V1__Tabeller.sql
+++ app/src/main/resources/db/migration/V1__Tabeller.sql
@@ -13,17 +13,77 @@ $$;
 
 CREATE TABLE IF NOT EXISTS søker
 (
-    id       UUID PRIMARY KEY,
-    ident    VARCHAR NOT NULL UNIQUE,
-    tilstand VARCHAR NOT NULL
+    id          UUID PRIMARY KEY,
+    ident       VARCHAR                  NOT NULL UNIQUE,
+    tilstand    VARCHAR                  NOT NULL,
+    sist_endret TIMESTAMP WITH TIME ZONE NOT NULL,
+    opprettet   TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS søknad
 (
-    id              UUID PRIMARY KEY,
-    søker_id        UUID                     NOT NULL REFERENCES søker (id),
-    journalpost_id  BIGINT                   NOT NULL,
-    dokumentinfo_id BIGINT                   NOT NULL,
-    opprettet_dato  TIMESTAMP WITH TIME ZONE NOT NULL,
-    created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+    id                  UUID PRIMARY KEY,
+    søker_id            UUID                     NOT NULL REFERENCES søker (id),
+    søknad_id           VARCHAR                  NOT NULL,
+    fornavn             VARCHAR                  NULL,
+    etternavn           VARCHAR                  NULL,
+    deltar_kvp          BOOLEAN                  NOT NULL,
+    deltar_intro        BOOLEAN                  NULL,
+    institusjon_opphold BOOLEAN                  NULL,
+    institusjon_type    VARCHAR                  NULL,
+    fritekst            VARCHAR                  NULL,
+    journalpost_id      BIGINT                   NOT NULL,
+    dokumentinfo_id     BIGINT                   NOT NULL,
+    tidsstempel_kilde   TIMESTAMP WITH TIME ZONE NOT NULL,
+    tidsstempel_hos_oss TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS barnetillegg
+(
+    id        UUID PRIMARY KEY,
+    søknad_id UUID    NOT NULL REFERENCES søknad (id),
+    ident     VARCHAR NOT NULL,
+    fornavn   VARCHAR NULL,
+    etternavn VARCHAR NULL,
+    alder     INT     NOT NULL,
+    land      VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS brukertiltak
+(
+    id            UUID PRIMARY KEY,
+    søknad_id     UUID    NOT NULL REFERENCES søknad (id),
+    tiltakstype   VARCHAR NULL,
+    arrangoernavn VARCHAR NULL,
+    beskrivelse   VARCHAR NULL,
+    fom           DATE    NULL,
+    tom           DATE    NULL,
+    adresse       VARCHAR NULL,
+    postnummer    VARCHAR NULL,
+    antall_dager  INT     NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS arenatiltak
+(
+    id                      UUID PRIMARY KEY,
+    søknad_id               UUID    NOT NULL REFERENCES søknad (id),
+    arena_id                VARCHAR NULL,
+    arrangoer               VARCHAR NULL,
+    har_sluttdato_fra_arena BOOLEAN NULL,
+    navn                    VARCHAR NULL,
+    er_i_endre_status       BOOLEAN NULL,
+    opprinnelig_startdato   DATE    NULL,
+    opprinnelig_sluttdato   DATE    NULL,
+    startdato               DATE    NULL,
+    sluttdato               DATE    NULL
+);
+
+CREATE TABLE IF NOT EXISTS trygdogpensjon
+(
+    id        UUID PRIMARY KEY,
+    søknad_id UUID    NOT NULL REFERENCES søknad (id),
+    utbetaler VARCHAR NOT NULL,
+    prosent   INT     NULL,
+    fom       DATE    NOT NULL,
+    tom       DATE    NULL
 );
