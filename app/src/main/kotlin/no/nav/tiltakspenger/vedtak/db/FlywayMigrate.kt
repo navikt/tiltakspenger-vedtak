01b4dff8bc2165646713f5799cd3d38a4d5ee2f3
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-09-14 10:25:08 +0200

    Fix 'flyway.cleanDisabled'-problem; Flyway was not able to migrate

--- app/src/main/kotlin/no/nav/tiltakspenger/vedtak/db/FlywayMigrate.kt
+++ app/src/main/kotlin/no/nav/tiltakspenger/vedtak/db/FlywayMigrate.kt
@@ -5,7 +5,6 @@ import org.flywaydb.core.Flyway
 fun flywayMigrate() {
     Flyway.configure()
         .dataSource(DataSource.hikariDataSource)
-        .cleanOnValidationError(true)
         .load()
         .migrate()
 }
