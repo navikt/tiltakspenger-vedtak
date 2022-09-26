c493bf7ca18a9f279df114d94faefb52baaad7d5
Richard Martinsen <github@martinsen.org>
2022-09-26 23:02:24 +0200

    Legg til noen fromDb tester

--- app/src/test/kotlin/no/nav/tiltakspenger/vedtak/db/UlidTest.kt
+++ app/src/test/kotlin/no/nav/tiltakspenger/vedtak/db/UlidTest.kt
@@ -1,5 +1,6 @@
 package no.nav.tiltakspenger.vedtak.db
 
+import io.kotest.assertions.throwables.shouldThrowWithMessage
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -19,5 +20,28 @@ internal class UlidTest {
         val ulid = Ulid.new(prefix)
         val ulid2 = Ulid.fromDb("${ulid.prefixPart()}_${ulid.ulidPart()}")
         assertEquals(ulid, ulid2)
+    }
+
+    @Test
+    fun `test fromDb negativ test`() {
+        val utenGyldigSkilletegn = "HH"
+        shouldThrowWithMessage<IllegalArgumentException>("Ikke gyldig Id, skal bestå av to deler skilt med _") {
+            Ulid.fromDb(utenGyldigSkilletegn)
+        }
+
+        val utenPrefiks = "_HH"
+        shouldThrowWithMessage<IllegalArgumentException>("Ikke gyldig Id, prefiks er tom") {
+            Ulid.fromDb(utenPrefiks)
+        }
+
+        val utenUlid = "HH_"
+        shouldThrowWithMessage<IllegalArgumentException>("Ikke gyldig Id, ulid er ugyldig") {
+            Ulid.fromDb(utenUlid)
+        }
+
+        val ugyldigUlid = "HH_UU_JJ"
+        shouldThrowWithMessage<IllegalArgumentException>("Ikke gyldig Id, skal bestå av prefiks + ulid") {
+            Ulid.fromDb(ugyldigUlid)
+        }
     }
 }
