9a382764cb0da1a7d2fd3bc7e0da6ec42f9b84b3
Richard Martinsen <github@martinsen.org>
2022-09-27 13:35:31 +0200

    Endre oppdater tilstand til .name også

--- app/src/main/resources/db/migration/V1__Tabeller.sql
+++ app/src/main/resources/db/migration/V1__Tabeller.sql
@@ -92,34 +92,34 @@ CREATE TABLE trygdogpensjon
 
 CREATE TABLE personopplysninger
 (
-    id                  UUID        PRIMARY KEY,
-    søker_id            UUID        NOT NULL REFERENCES søker (id),
-    ident               VARCHAR     NOT NULL UNIQUE,
-    fødselsdato         DATE        NOT NULL,
-    fornavn             VARCHAR     NOT NULL,
-    mellomnavn          VARCHAR     NULL,
-    etternavn           VARCHAR     NOT NULL,
-    fortrolig           BOOLEAN     NOT NULL,
-    strengtFortrolig    BOOLEAN     NOT NULL,
-    skjermet            BOOLEAN     NULL,
-    innhentet           TIMESTAMP WITH TIME ZONE NULL
+    id               UUID PRIMARY KEY,
+    søker_id         UUID                     NOT NULL REFERENCES søker (id),
+    ident            VARCHAR                  NOT NULL UNIQUE,
+    fødselsdato      DATE                     NOT NULL,
+    fornavn          VARCHAR                  NOT NULL,
+    mellomnavn       VARCHAR                  NULL,
+    etternavn        VARCHAR                  NOT NULL,
+    fortrolig        BOOLEAN                  NOT NULL,
+    strengtFortrolig BOOLEAN                  NOT NULL,
+    skjermet         BOOLEAN                  NULL,
+    innhentet        TIMESTAMP WITH TIME ZONE NULL
 );
 
 CREATE TABLE tiltaksaktivitet
 (
-    id                      UUID        PRIMARY KEY,
-    søker_id                UUID        NOT NULL REFERENCES søker (id),
-    tiltak                  VARCHAR     NOT NULL,
-    aktivitet_id            VARCHAR     NOT NULL,
-    tiltak_lokalt_navn      VARCHAR     NULL,
-    arrangør                VARCHAR     NULL,
-    bedriftsnummer          VARCHAR     NULL,
-    deltakelse_periode_fom  DATE        NULL,
-    deltakelse_periode_tom  DATE        NULL,
-    deltakelse_prosent      FLOAT       NULL,
-    deltaker_status         VARCHAR     NOT NULL,
-    status_sist_endret      DATE        NULL,
-    begrunnelse_innsøking   VARCHAR     NULL,
-    antall_dager_per_uke    FLOAT       NULL,
-    tidsstempel_hos_oss     TIMESTAMP WITH TIME ZONE NULL
+    id                     UUID PRIMARY KEY,
+    søker_id               UUID                     NOT NULL REFERENCES søker (id),
+    tiltak                 VARCHAR                  NOT NULL,
+    aktivitet_id           VARCHAR                  NOT NULL,
+    tiltak_lokalt_navn     VARCHAR                  NULL,
+    arrangør               VARCHAR                  NULL,
+    bedriftsnummer         VARCHAR                  NULL,
+    deltakelse_periode_fom DATE                     NULL,
+    deltakelse_periode_tom DATE                     NULL,
+    deltakelse_prosent     FLOAT                    NULL,
+    deltaker_status        VARCHAR                  NOT NULL,
+    status_sist_endret     DATE                     NULL,
+    begrunnelse_innsøking  VARCHAR                  NULL,
+    antall_dager_per_uke   FLOAT                    NULL,
+    tidsstempel_hos_oss    TIMESTAMP WITH TIME ZONE NULL
 );
