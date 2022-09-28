b95436dfb85c7429a0ae4fd274f1a53fb5ac479a
Hågen Hasle <haagenhasle@gmail.com>
2022-09-28 17:44:04 +0200

    Synket opp PersonopplysningerDTO med modellendringer i tiltakspenger-fakta-person

--- app/src/main/kotlin/no/nav/tiltakspenger/vedtak/rivers/BarnDTO.kt
+++ app/src/main/kotlin/no/nav/tiltakspenger/vedtak/rivers/BarnDTO.kt
@@ -3,8 +3,18 @@ package no.nav.tiltakspenger.vedtak.rivers
 import java.time.LocalDate
 
 data class BarnDTO(
+    val ident: String,
+    val fornavn: String,
+    val mellomnavn: String?,
+    val etternavn: String,
+    val fødselsdato: LocalDate,
+    val adressebeskyttelseGradering: AdressebeskyttelseGradering,
+)
+
+data class BarnUtenFolkeregisteridentifikatorDTO(
     val fornavn: String?,
     val mellomnavn: String?,
     val etternavn: String?,
-    val fødselsdato: LocalDate?
+    val fødselsdato: LocalDate?,
+    val statsborgerskap: String?,
 )
