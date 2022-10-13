4c5c2833655dd414c941a8191a1bdc796fcba2b2
Richard Martinsen <github@martinsen.org>
2022-10-13 13:11:15 +0200

    teste på periode
    Co-authored-by: erlendev <Erlend.Edvardsen.Vige@nav.no>>
    Co-authored-by: haagenhasle <Hagen.Hasle@nav.no>
    Co-authored-by: pjwalstrom <Per.Jorgen.Walstrom@nav.no>

--- domene/src/test/kotlin/no/nav/tiltakspenger/vilkårsvurdering/IntroProgrammetVilkårsvurderingTest.kt
+++ domene/src/test/kotlin/no/nav/tiltakspenger/vilkårsvurdering/IntroProgrammetVilkårsvurderingTest.kt
@@ -1,6 +1,7 @@
 package no.nav.tiltakspenger.vilkårsvurdering
 
 import io.kotest.matchers.shouldBe
+import no.nav.tiltakspenger.domene.Periode
 import no.nav.tiltakspenger.domene.januar
 import no.nav.tiltakspenger.felles.SøknadId
 import no.nav.tiltakspenger.objectmothers.arenaTiltak
@@ -13,6 +14,11 @@ internal class IntroProgrammetVilkårsvurderingTest {
 
     @Test
     fun `Kunne sende inn en søknad i vilkårsvurdering`() {
+        val forventetPeriode = Periode(
+            fra = 1 januar(2022),
+            til = 31 januar(2022),
+        )
+
         val søknad = Søknad(
             id = SøknadId.random(),
             søknadId = "1234",
@@ -40,6 +46,7 @@ internal class IntroProgrammetVilkårsvurderingTest {
         val introProgrammetVilkårsvurdering = IntroProgrammetVilkårsvurdering(søknad = søknad)
 
         introProgrammetVilkårsvurdering.vurderinger.first().kilde shouldBe "Søknad"
+        introProgrammetVilkårsvurdering.vurderinger.first().periode shouldBe forventetPeriode
     }
 
 }
