6cb3c2f432c3f307635adafe531632908b362a06
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-10-13 17:59:54 +0200

    Replace an 'if' with a 'when'

--- domene/src/main/kotlin/no/nav/tiltakspenger/vilkårsvurdering/KommunaleYtelserVilkårsvurdering.kt
+++ domene/src/main/kotlin/no/nav/tiltakspenger/vilkårsvurdering/KommunaleYtelserVilkårsvurdering.kt
@@ -1,23 +1,22 @@
 package no.nav.tiltakspenger.vilkårsvurdering
 
+import no.nav.tiltakspenger.vilkårsvurdering.Utfall.IKKE_OPPFYLT
+import no.nav.tiltakspenger.vilkårsvurdering.Utfall.KREVER_MANUELL_VURDERING
+import no.nav.tiltakspenger.vilkårsvurdering.Utfall.OPPFYLT
+
 class KommunaleYtelserVilkårsvurdering(
     private val introProgrammetVilkårsvurdering: IntroProgrammetVilkårsvurdering,
     private val kvpVilkårsvurdering: KVPVilkårsvurdering
 ) {
     fun samletUtfall(): Utfall {
-        return if (
-            introProgrammetVilkårsvurdering.samletUtfall() == Utfall.IKKE_OPPFYLT ||
-            kvpVilkårsvurdering.samletUtfall() == Utfall.IKKE_OPPFYLT
-        ) {
-            Utfall.IKKE_OPPFYLT
-        } else if (
-            introProgrammetVilkårsvurdering.samletUtfall() == Utfall.KREVER_MANUELL_VURDERING ||
-            kvpVilkårsvurdering.samletUtfall() == Utfall.KREVER_MANUELL_VURDERING
-        ) {
-            Utfall.KREVER_MANUELL_VURDERING
-        } else {
-            Utfall.OPPFYLT
+        return when {
+            introProgrammetVilkårsvurdering.samletUtfall() == IKKE_OPPFYLT ||
+                    kvpVilkårsvurdering.samletUtfall() == IKKE_OPPFYLT -> IKKE_OPPFYLT
+
+            introProgrammetVilkårsvurdering.samletUtfall() == KREVER_MANUELL_VURDERING ||
+                    kvpVilkårsvurdering.samletUtfall() == KREVER_MANUELL_VURDERING -> KREVER_MANUELL_VURDERING
+
+            else -> OPPFYLT
         }
     }
-
 }
