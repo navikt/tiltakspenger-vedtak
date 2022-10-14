043b79f39a69a873d6f23c76075f1a101aa988ea
Hågen Hasle <haagenhasle@gmail.com>
2022-10-14 15:06:45 +0200

    La til forretningslogikk, lagde test for manuell vurdering
    Co-authored-by: richardmartinsen <Richard.Martinsen@nav.no>

--- domene/src/main/kotlin/no/nav/tiltakspenger/vilkårsvurdering/AAPVilkårsvurdering.kt
+++ domene/src/main/kotlin/no/nav/tiltakspenger/vilkårsvurdering/AAPVilkårsvurdering.kt
@@ -2,21 +2,42 @@ package no.nav.tiltakspenger.vilkårsvurdering
 
 import no.nav.tiltakspenger.domene.Periode
 import no.nav.tiltakspenger.vedtak.YtelseSak
+import java.time.LocalDate
 
 class AAPVilkårsvurdering(
     private val ytelser: List<YtelseSak>,
     private val vurderingsperiode: Periode,
 ) {
     val lovReferanse: Lovreferanse = Lovreferanse.AAP
-    private val ytelseVurdering = lagYtelseVurdering()
+    private val ytelseVurderinger = lagYtelseVurderinger()
 
-    private fun lagYtelseVurdering() = Vurdering(
-        kilde = "Arena",
-        fom = ytelser.first().fomGyldighetsperiode.toLocalDate(),
-        tom = ytelser.first().fomGyldighetsperiode.toLocalDate(),
-        utfall = Utfall.KREVER_MANUELL_VURDERING,
-        detaljer = "",
-    )
+    private fun lagYtelseVurderinger(): List<Vurdering> =
+        ytelser
+            .filter {
+                Periode(
+                    it.fomGyldighetsperiode.toLocalDate(),
+                    (it.tomGyldighetsperiode?.toLocalDate() ?: LocalDate.MAX)
+                ).overlapperMed(vurderingsperiode)
+            }.filter { it.ytelsestype == YtelseSak.YtelseSakYtelsetype.AA }
+            .map {
+                Vurdering(
+                    kilde = "Arena",
+                    fom = it.fomGyldighetsperiode.toLocalDate(),
+                    tom = it.tomGyldighetsperiode?.toLocalDate(),
+                    utfall = Utfall.IKKE_OPPFYLT,
+                    detaljer = "",
+                )
+            }.ifEmpty {
+                listOf(
+                    Vurdering(
+                        kilde = "Arena",
+                        fom = null,
+                        tom = null,
+                        utfall = Utfall.OPPFYLT,
+                        detaljer = "",
+                    )
+                )
+            }
 
-    fun vurderinger(): List<Vurdering> = listOfNotNull(ytelseVurdering)
+    fun vurderinger(): List<Vurdering> = ytelseVurderinger
 }
--- domene/src/test/kotlin/no/nav/tiltakspenger/vilkårsvurdering/AAPVilkårsvurderingTest.kt
+++ domene/src/test/kotlin/no/nav/tiltakspenger/vilkårsvurdering/AAPVilkårsvurderingTest.kt
@@ -1,9 +1,12 @@
 package no.nav.tiltakspenger.vilkårsvurdering
 
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.shouldBe
 import no.nav.tiltakspenger.domene.Periode
 import no.nav.tiltakspenger.domene.februar
+import no.nav.tiltakspenger.domene.januar
 import no.nav.tiltakspenger.domene.januarDateTime
+import no.nav.tiltakspenger.domene.mars
 import no.nav.tiltakspenger.domene.marsDateTime
 import no.nav.tiltakspenger.objectmothers.ytelseSak
 import no.nav.tiltakspenger.vedtak.YtelseSak
@@ -12,7 +15,7 @@ import org.junit.jupiter.api.Test
 internal class AAPVilkårsvurderingTest {
 
     @Test
-    fun `En vilkårsvurdering har en søknad`() {
+    fun `skal bli oppfylt`() {
         val ytelser = listOf(
             ytelseSak(
                 fomGyldighetsperiode = 1.januarDateTime(2022),
@@ -34,6 +37,99 @@ internal class AAPVilkårsvurderingTest {
         aapVilkårsvurdering.vurderinger().first().fom shouldBe null
         aapVilkårsvurdering.vurderinger().first().tom shouldBe null
         aapVilkårsvurdering.vurderinger().first().utfall shouldBe Utfall.OPPFYLT
+        aapVilkårsvurdering.lovReferanse.paragraf shouldBe "§7"
+        aapVilkårsvurdering.lovReferanse.ledd shouldBe "1"
+    }
+
+    @Test
+    fun `skal ikke bli oppfylt i begge ender`() {
+        val ytelser = listOf(
+            ytelseSak(
+                fomGyldighetsperiode = 1.januarDateTime(2022),
+                tomGyldighetsperiode = 31.januarDateTime(2022),
+                ytelsestype = YtelseSak.YtelseSakYtelsetype.AA
+            ),
+            ytelseSak(
+                fomGyldighetsperiode = 1.marsDateTime(2022),
+                tomGyldighetsperiode = 31.marsDateTime(2022),
+                ytelsestype = YtelseSak.YtelseSakYtelsetype.AA
+            ),
+        )
+        val vurderingsperiode = Periode(19.januar(2022), 28.mars(2022))
+
+        val aapVilkårsvurdering =
+            AAPVilkårsvurdering(ytelser = ytelser, vurderingsperiode = vurderingsperiode)
+
+        aapVilkårsvurdering.vurderinger() shouldContainExactlyInAnyOrder listOf(
+            Vurdering(
+                kilde = "Arena",
+                fom = 1.januar(2022),
+                tom = 31.januar(2022),
+                utfall = Utfall.IKKE_OPPFYLT,
+                detaljer = ""
+            ),
+            Vurdering(
+                kilde = "Arena",
+                fom = 1.mars(2022),
+                tom = 31.mars(2022),
+                utfall = Utfall.IKKE_OPPFYLT,
+                detaljer = ""
+            ),
+        )
+        aapVilkårsvurdering.lovReferanse.paragraf shouldBe "§7"
+        aapVilkårsvurdering.lovReferanse.ledd shouldBe "1"
+    }
+
+    @Test
+    fun `skal bli oppfylt fordi den overstyres manuelt`() {
+        val ytelser = listOf(
+            ytelseSak(
+                fomGyldighetsperiode = 1.januarDateTime(2022),
+                tomGyldighetsperiode = 31.januarDateTime(2022),
+                ytelsestype = YtelseSak.YtelseSakYtelsetype.AA
+            ),
+            ytelseSak(
+                fomGyldighetsperiode = 1.marsDateTime(2022),
+                tomGyldighetsperiode = 31.marsDateTime(2022),
+                ytelsestype = YtelseSak.YtelseSakYtelsetype.AA
+            ),
+        )
+        val vurderingsperiode = Periode(19.januar(2022), 28.mars(2022))
+
+        val aapVilkårsvurdering =
+            AAPVilkårsvurdering(ytelser = ytelser, vurderingsperiode = vurderingsperiode)
+
+        aapVilkårsvurdering.settManuellVurdering(
+            fom = 19.januar(2022),
+            tom = 28.mars(2022),
+            utfall = Utfall.OPPFYLT,
+            detaljer = "",
+        )
+
+        aapVilkårsvurdering.vurderinger() shouldContainExactlyInAnyOrder listOf(
+            Vurdering(
+                kilde = "Arena",
+                fom = 1.januar(2022),
+                tom = 31.januar(2022),
+                utfall = Utfall.IKKE_OPPFYLT,
+                detaljer = ""
+            ),
+            Vurdering(
+                kilde = "Saksbehandler",
+                fom = 1.mars(2022),
+                tom = 31.mars(2022),
+                utfall = Utfall.IKKE_OPPFYLT,
+                detaljer = ""
+            ),
+            Vurdering(
+                kilde = "Arena",
+                fom = 19.januar(2022),
+                tom = 28.mars(2022),
+                utfall = Utfall.OPPFYLT,
+                detaljer = ""
+            )
+        )
+        aapVilkårsvurdering.samletVurdering shouldBe Utfall.OPPFYLT
         aapVilkårsvurdering.lovReferanse.paragraf shouldBe "§7"
         aapVilkårsvurdering.lovReferanse.ledd shouldBe "1"
     }
