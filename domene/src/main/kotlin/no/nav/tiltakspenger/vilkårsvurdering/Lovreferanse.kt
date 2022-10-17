df92b4797b5f03fb71f0aec7c0b58bb78de9028b
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-10-17 15:25:25 +0200

    Extract Lovreferanse to own file
    Co-authored-by: richardmartinsen <Richard.Martinsen@nav.no>

---
+++ domene/src/main/kotlin/no/nav/tiltakspenger/vilkårsvurdering/Lovreferanse.kt
@@ -0,0 +1,9 @@
+package no.nav.tiltakspenger.vilkårsvurdering
+
+enum class Lovreferanse(val lovverk: String, val paragraf: String, val ledd: String?, val beskrivelse: String) {
+    AAP("Tiltakspengeforskriften", "§7", "1", "Forholdet til andre ytelser"),
+    DAGPENGER("Tiltakspengeforskriften", "§7", "1", "Forholdet til andre ytelser"),
+    INTROPROGRAMMET("Tiltakspengeforskriften", "§7", "3", "Forholdet til andre ytelser"),
+    KVP("Tiltakspengeforskriften", "§7", "3", "Forholdet til andre ytelser"),
+    KOMMUNALE_YTELSER("Tiltakspengeforskriften", "§7", null, "Forholdet til andre ytelser"),
+}
