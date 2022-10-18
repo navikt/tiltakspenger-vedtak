71bcdc88835398c34939b9f2049b0f3b82509570
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-10-18 17:08:33 +0200

    Delete redundant test and rename another to reflect it's true purpose

--- domene/src/test/kotlin/no/nav/tiltakspenger/vilkårsvurdering/StatligYtelseVilkårsvurderingTest.kt
+++ domene/src/test/kotlin/no/nav/tiltakspenger/vilkårsvurdering/StatligYtelseVilkårsvurderingTest.kt
@@ -15,7 +15,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
-internal class AAPVilkårsvurderingTest {
+internal class StatligYtelseVilkårsvurderingTest {
 
     companion object {
         @Suppress("UnusedPrivateMember")
