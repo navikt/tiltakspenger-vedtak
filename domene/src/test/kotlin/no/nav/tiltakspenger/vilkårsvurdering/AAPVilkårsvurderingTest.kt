5d718239dde8ffdd6c74395de660b513119751cb
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-10-18 17:06:03 +0200

    Parameterize AAPVilkårsvurderingTest to also test Dagpenger

--- domene/src/test/kotlin/no/nav/tiltakspenger/vilkårsvurdering/AAPVilkårsvurderingTest.kt
+++ domene/src/test/kotlin/no/nav/tiltakspenger/vilkårsvurdering/AAPVilkårsvurderingTest.kt
@@ -10,64 +10,57 @@ import no.nav.tiltakspenger.domene.mars
 import no.nav.tiltakspenger.domene.marsDateTime
 import no.nav.tiltakspenger.objectmothers.ytelseSak
 import no.nav.tiltakspenger.vedtak.YtelseSak
-import org.junit.jupiter.api.Test
+import no.nav.tiltakspenger.vedtak.YtelseSak.YtelseSakYtelsetype.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class AAPVilkårsvurderingTest {
 
-    @Test
-    fun `skal bli oppfylt`() {
-        val ytelser = listOf(
+    companion object {
+        @Suppress("UnusedPrivateMember")
+        @JvmStatic
+        private fun statligYtelseKort() = arguments(Periode(19.februar(2022), 28.februar(2022)))
+
+        @Suppress("UnusedPrivateMember")
+        @JvmStatic
+        private fun statligYtelseLang() = arguments(Periode(19.januar(2022), 28.mars(2022)))
+
+        private fun arguments(vurderingsperiode: Periode) = listOf(
+            Arguments.of(StatligYtelseVilkårsvurdering.AAPVilkårsvurdering(ytelser(AA), vurderingsperiode)),
+            Arguments.of(StatligYtelseVilkårsvurdering.DagpengerVilkårsvurdering(ytelser(DAGP), vurderingsperiode))
+        )
+
+        private fun ytelser(type: YtelseSak.YtelseSakYtelsetype) = listOf(
             ytelseSak(
                 fomGyldighetsperiode = 1.januarDateTime(2022),
                 tomGyldighetsperiode = 31.januarDateTime(2022),
-                ytelsestype = YtelseSak.YtelseSakYtelsetype.AA
+                ytelsestype = type
             ),
             ytelseSak(
                 fomGyldighetsperiode = 1.marsDateTime(2022),
                 tomGyldighetsperiode = 31.marsDateTime(2022),
-                ytelsestype = YtelseSak.YtelseSakYtelsetype.AA
+                ytelsestype = type
             ),
         )
-        val vurderingsperiode = Periode(19.februar(2022), 28.februar(2022))
-
-        val aapVilkårsvurdering =
-            StatligYtelseVilkårsvurdering.AAPVilkårsvurdering(
-                ytelser = ytelser,
-                vurderingsperiode = vurderingsperiode
-            )
-
-        aapVilkårsvurdering.samletUtfall() shouldBe Utfall.OPPFYLT
-        aapVilkårsvurdering.vurderinger().first().kilde shouldBe "Arena"
-        aapVilkårsvurdering.vurderinger().first().fom shouldBe null
-        aapVilkårsvurdering.vurderinger().first().tom shouldBe null
-        aapVilkårsvurdering.vurderinger().first().utfall shouldBe Utfall.OPPFYLT
-        aapVilkårsvurdering.lovreferanse.paragraf shouldBe "§7"
-        aapVilkårsvurdering.lovreferanse.ledd shouldBe "1"
     }
 
-    @Test
-    fun `skal ikke bli oppfylt i begge ender`() {
-        val ytelser = listOf(
-            ytelseSak(
-                fomGyldighetsperiode = 1.januarDateTime(2022),
-                tomGyldighetsperiode = 31.januarDateTime(2022),
-                ytelsestype = YtelseSak.YtelseSakYtelsetype.AA
-            ),
-            ytelseSak(
-                fomGyldighetsperiode = 1.marsDateTime(2022),
-                tomGyldighetsperiode = 31.marsDateTime(2022),
-                ytelsestype = YtelseSak.YtelseSakYtelsetype.AA
-            ),
-        )
-        val vurderingsperiode = Periode(19.januar(2022), 28.mars(2022))
+    @ParameterizedTest
+    @MethodSource("statligYtelseKort")
+    fun `skal bli oppfylt`(statligVilkårsvurdering: StatligYtelseVilkårsvurdering) {
+        statligVilkårsvurdering.samletUtfall() shouldBe Utfall.OPPFYLT
+        statligVilkårsvurdering.vurderinger().first().kilde shouldBe "Arena"
+        statligVilkårsvurdering.vurderinger().first().fom shouldBe null
+        statligVilkårsvurdering.vurderinger().first().tom shouldBe null
+        statligVilkårsvurdering.vurderinger().first().utfall shouldBe Utfall.OPPFYLT
+        statligVilkårsvurdering.lovreferanse.paragraf shouldBe "§7"
+        statligVilkårsvurdering.lovreferanse.ledd shouldBe "1"
+    }
 
-        val aapVilkårsvurdering =
-            StatligYtelseVilkårsvurdering.AAPVilkårsvurdering(
-                ytelser = ytelser,
-                vurderingsperiode = vurderingsperiode
-            )
-
-        aapVilkårsvurdering.vurderinger() shouldContainExactlyInAnyOrder listOf(
+    @ParameterizedTest
+    @MethodSource("statligYtelseLang")
+    fun `skal ikke bli oppfylt i begge ender`(statligVilkårsvurdering: StatligYtelseVilkårsvurdering) {
+        statligVilkårsvurdering.vurderinger() shouldContainExactlyInAnyOrder listOf(
             Vurdering(
                 kilde = "Arena",
                 fom = 1.januar(2022),
@@ -83,41 +76,22 @@ internal class AAPVilkårsvurderingTest {
                 detaljer = ""
             ),
         )
-        aapVilkårsvurdering.samletUtfall() shouldBe Utfall.IKKE_OPPFYLT
-        aapVilkårsvurdering.lovreferanse.paragraf shouldBe "§7"
-        aapVilkårsvurdering.lovreferanse.ledd shouldBe "1"
+        statligVilkårsvurdering.samletUtfall() shouldBe Utfall.IKKE_OPPFYLT
+        statligVilkårsvurdering.lovreferanse.paragraf shouldBe "§7"
+        statligVilkårsvurdering.lovreferanse.ledd shouldBe "1"
     }
 
-    @Test
-    fun `skal bli oppfylt fordi den overstyres manuelt`() {
-        val ytelser = listOf(
-            ytelseSak(
-                fomGyldighetsperiode = 1.januarDateTime(2022),
-                tomGyldighetsperiode = 31.januarDateTime(2022),
-                ytelsestype = YtelseSak.YtelseSakYtelsetype.AA
-            ),
-            ytelseSak(
-                fomGyldighetsperiode = 1.marsDateTime(2022),
-                tomGyldighetsperiode = 31.marsDateTime(2022),
-                ytelsestype = YtelseSak.YtelseSakYtelsetype.AA
-            ),
-        )
-        val vurderingsperiode = Periode(19.januar(2022), 28.mars(2022))
-
-        val aapVilkårsvurdering =
-            StatligYtelseVilkårsvurdering.AAPVilkårsvurdering(
-                ytelser = ytelser,
-                vurderingsperiode = vurderingsperiode
-            )
-
-        aapVilkårsvurdering.settManuellVurdering(
+    @ParameterizedTest
+    @MethodSource("statligYtelseLang")
+    fun `skal bli oppfylt fordi den overstyres manuelt`(statligVilkårsvurdering: StatligYtelseVilkårsvurdering) {
+        statligVilkårsvurdering.settManuellVurdering(
             fom = 19.januar(2022),
             tom = 28.mars(2022),
             utfall = Utfall.OPPFYLT,
             detaljer = "",
         )
 
-        aapVilkårsvurdering.vurderinger() shouldContainExactlyInAnyOrder listOf(
+        statligVilkårsvurdering.vurderinger() shouldContainExactlyInAnyOrder listOf(
             Vurdering(
                 kilde = "Arena",
                 fom = 1.januar(2022),
@@ -140,8 +114,8 @@ internal class AAPVilkårsvurderingTest {
                 detaljer = ""
             )
         )
-        aapVilkårsvurdering.samletUtfall() shouldBe Utfall.OPPFYLT
-        aapVilkårsvurdering.lovreferanse.paragraf shouldBe "§7"
-        aapVilkårsvurdering.lovreferanse.ledd shouldBe "1"
+        statligVilkårsvurdering.samletUtfall() shouldBe Utfall.OPPFYLT
+        statligVilkårsvurdering.lovreferanse.paragraf shouldBe "§7"
+        statligVilkårsvurdering.lovreferanse.ledd shouldBe "1"
     }
 }
