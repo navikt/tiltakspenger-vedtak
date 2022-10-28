5045587260e09f30247e8d23f428ae836f23b5f1
Richard Martinsen <github@martinsen.org>
2022-10-28 10:36:23 +0200

    Legg til testdata

--- app/src/main/resources/db/local-migration/V1000__Testdata.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE DUMMY
-(
-    id          VARCHAR PRIMARY KEY,
-    ident       VARCHAR                  NOT NULL UNIQUE,
-    tilstand    VARCHAR                  NOT NULL,
-    sist_endret TIMESTAMP WITH TIME ZONE NOT NULL,
-    opprettet   TIMESTAMP WITH TIME ZONE NOT NULL
-);
---
+++ app/src/main/resources/db/local-migration/V1006__søknad_vedlegg.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.søknad_vedlegg (id, søknad_id, journalpost_id, dokumentinfo_id, filnavn) VALUES ('vedlegg_01GG9PDD7N6V560T6YEPZM1HPN', 'soknad_01GG9PCV22RY6Z1ASHMVWZ14PA', '573787925', '599051166', 'H8');
+INSERT INTO public.søknad_vedlegg (id, søknad_id, journalpost_id, dokumentinfo_id, filnavn) VALUES ('vedlegg_01GG9XJ9MKRR3P0RRT1AKH1099', 'soknad_01GG9XJ6VQV26F0WZV9XHN1QEY', '573787965', '599051216', 'H8');
+INSERT INTO public.søknad_vedlegg (id, søknad_id, journalpost_id, dokumentinfo_id, filnavn) VALUES ('vedlegg_01GGC644KZGPS51B46K7890SXN', 'soknad_01GGC63ZDTW7QK1WG8J0JA0X95', '573788152', '599051449', 'dette er et annet vedlegg');
