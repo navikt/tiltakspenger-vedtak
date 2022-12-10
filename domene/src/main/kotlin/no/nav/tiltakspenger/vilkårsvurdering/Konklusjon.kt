6f6ad61901ed6310d6d6dc542ec998b956f4bccb
Hågen Hasle <haagenhasle@gmail.com>
2022-12-10 16:17:30 +0100

    Lagt til litt kommentarer og en test til

--- domene/src/main/kotlin/no/nav/tiltakspenger/vilkårsvurdering/Konklusjon.kt
+++ domene/src/main/kotlin/no/nav/tiltakspenger/vilkårsvurdering/Konklusjon.kt
@@ -7,46 +7,68 @@ import no.nav.tiltakspenger.domene.trekkFra
 
 fun List<Vurdering>.konklusjonFor(vurderingsperiode: Periode): Konklusjon {
 
+    val oppfylteVurderinger = this.filter { it.utfall == Utfall.OPPFYLT }
     val ikkeOppfyltVurderinger = this.filter { it.utfall == Utfall.IKKE_OPPFYLT }
     val manuelleVurderinger = this.filter { it.utfall == Utfall.KREVER_MANUELL_VURDERING }
-    val oppfyllteVurderinger = this.filter { it.utfall == Utfall.OPPFYLT }
 
+    // For at vurderingsperioden skal være oppfylt i sin helhet, så må alle vilkår være oppfylt
     if (this.all { it.utfall == Utfall.OPPFYLT }) {
         return Konklusjon.Oppfylt(vurderingsperiode to this.map { it.vilkår }.toSet())
     }
 
-    val ikkeOppfyltePerioder = ikkeOppfyltVurderinger.map { it.periode() }.leggSammen()
+    val ikkeOppfyltePerioderIVurderingsperioden = ikkeOppfyltVurderinger
+        .map { it.periode() }
+        .leggSammen()
+        .filter { it.overlapperMed(vurderingsperiode) }
+        .mapNotNull { it.overlappendePeriode(vurderingsperiode) }
 
-    if (ikkeOppfyltePerioder.any { it.inneholderHele(vurderingsperiode) }) {
+
+    //Hvis dette slår til, så skal ikkeOppfyltePerioderIVurderingsperioden.size være 1:
+    if (ikkeOppfyltePerioderIVurderingsperioden.any { it.inneholderHele(vurderingsperiode) }) {
+        // Vi skal ikke kunne få ikke-oppfylte vilkårsvurderinger hvor den ikke-oppfylte perioden
+        // er utenfor vurderingsperioden, da ville vilkåret vært oppfylt
+        // Vi tar derfor med oss alle vilkårene her.
+        // Merk at dette ikke tar høyde for at det kan være ulike vilkår som gjør at ulike deler av vurderingsperioden
+        // ikke er oppfylt, så detaljerte er vi ikke. Ønsker man at for en gitt periode, så skal det være homogent
+        // hvilke vilkår som har slått til, så må vi dele det opp mye mer.
         return Konklusjon.IkkeOppfylt(vurderingsperiode to ikkeOppfyltVurderinger.map { it.vilkår }.toSet())
     }
 
-    val manuellePerioder = manuelleVurderinger.map { it.periode() }.leggSammen().trekkFra(ikkeOppfyltePerioder)
+    val manuellePerioderIVurderingsperioden = manuelleVurderinger
+        .map { it.periode() }
+        .leggSammen()
+        .trekkFra(ikkeOppfyltePerioderIVurderingsperioden)
+        .filter { it.overlapperMed(vurderingsperiode) }
+        .mapNotNull { it.overlappendePeriode(vurderingsperiode) }
 
-    if (manuellePerioder.any { it.overlapperMed(vurderingsperiode) }) {
-        val resultat: Map<Periode, Set<Vilkår>> = manuellePerioder
-            .filter { it.overlapperMed(vurderingsperiode) }
-            .associate { mp ->
-                mp.overlappendePeriode(vurderingsperiode)!! to manuelleVurderinger
-                    .filter { it.periode().overlapperMed(mp) }
-                    .map { it.vilkår }.toSet()
+    if (manuellePerioderIVurderingsperioden.isNotEmpty()) {
+        // Selv om vi her deler det opp i flere perioder, så er det ikke nødvendigvis sånn at alle vilkårene
+        // gjelder for hele perioden her heller
+        val vilkårPerPeriode: Map<Periode, Set<Vilkår>> = manuellePerioderIVurderingsperioden
+            .associateWith { manuellPeriode ->
+                manuelleVurderinger
+                    .filter { it.periode().overlapperMed(manuellPeriode) }
+                    .map { it.vilkår }
+                    .toSet()
             }
-        return Konklusjon.KreverManuellBehandling(resultat)
+        return Konklusjon.KreverManuellBehandling(vilkårPerPeriode)
     }
 
-    val oppfyltePerioder: List<Periode> = vurderingsperiode.trekkFra(ikkeOppfyltePerioder)
+    val oppfyltePerioderIVurderingsperioden: List<Periode> =
+        vurderingsperiode.trekkFra(ikkeOppfyltePerioderIVurderingsperioden)
 
-    val oppfylte: List<Konklusjon.Oppfylt> = oppfyltePerioder
-        .mapNotNull { it.overlappendePeriode(vurderingsperiode) }
+    //Alle vilkårene er nødvendigvis oppfylt i de periodene som er oppfylt
+    val oppfylte: List<Konklusjon.Oppfylt> = oppfyltePerioderIVurderingsperioden
         .map { periode ->
-            Konklusjon.Oppfylt(periode to oppfyllteVurderinger.map { vurdering -> vurdering.vilkår }.toSet())
+            Konklusjon.Oppfylt(periode to oppfylteVurderinger.map { vurdering -> vurdering.vilkår }.toSet())
         }
-    val ikkeOppfylte: List<Konklusjon.IkkeOppfylt> = ikkeOppfyltePerioder
-        .mapNotNull { it.overlappendePeriode(vurderingsperiode) }
-        .map { periode ->
+    val ikkeOppfylte: List<Konklusjon.IkkeOppfylt> = ikkeOppfyltePerioderIVurderingsperioden
+        .map { ikkeOppfyltPeriode ->
             Konklusjon.IkkeOppfylt(
-                //Denne er ikke korrekt, må bare ta med de som faktisk gjelder den aktuelle perioden
-                periode to ikkeOppfyltVurderinger.map { vurdering -> vurdering.vilkår }.toSet()
+                ikkeOppfyltPeriode to ikkeOppfyltVurderinger
+                    .filter { it.periode().overlapperMed(ikkeOppfyltPeriode) }
+                    .map { vurdering -> vurdering.vilkår }
+                    .toSet()
             )
         }
     return Konklusjon.DelvisOppfylt(oppfylte, ikkeOppfylte)
--- domene/src/test/kotlin/no/nav/tiltakspenger/vilkårsvurdering/KonklusjonTest.kt
+++ domene/src/test/kotlin/no/nav/tiltakspenger/vilkårsvurdering/KonklusjonTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 internal class KonklusjonTest {
-    
+
     @Test
     fun `Skal få oppfylt når alle vurderinger er oppfylt`() {
         val vurderingsperiode = Periode(LocalDate.of(2022, 1, 1), LocalDate.of(2022, 1, 31))
@@ -27,55 +27,62 @@ internal class KonklusjonTest {
             .shouldBe(Konklusjon.Oppfylt(vurderingsperiode to vilkår.toSet()))
     }
 
-//    @Test
-//    fun `Teste range av ikke godkjente`() {
-//        val vurderingsperiode = Periode(LocalDate.of(2022, 1, 1), LocalDate.of(2022, 1, 31))
-//        val vilkår: List<Vilkår> = listOf(
-//            Vilkår.DAGPENGER,
-//            Vilkår.AAP,
-//            Vilkår.ALDERSPENSJON,
-//            Vilkår.FORELDREPENGER,
-//            Vilkår.OMSORGSPENGER,
-//            Vilkår.SYKEPENGER,
-//            Vilkår.KVP,
-//            Vilkår.INTROPROGRAMMET
-//        )
-//
-//        vilkår
-//            .map { Vurdering(it, "", null, null, Utfall.OPPFYLT, "") }
-//            .plus(
-//                Vurdering(
-//                    Vilkår.GJENLEVENDEPENSJON,
-//                    "",
-//                    LocalDate.of(2022, 2, 1),
-//                    LocalDate.of(2022, 2, 28),
-//                    Utfall.IKKE_OPPFYLT,
-//                    ""
-//                )
-//            ).
-//            plus(
-//                Vurdering(
-//                    Vilkår.GJENLEVENDEPENSJON,
-//                    "",
-//                    LocalDate.of(2022, 4, 1),
-//                    LocalDate.of(2022, 4, 30),
-//                    Utfall.IKKE_OPPFYLT,
-//                    ""
-//                )
-//            )
-//            .plus(
-//                Vurdering(
-//                    Vilkår.GJENLEVENDEPENSJON,
-//                    "",
-//                    LocalDate.of(2022, 2, 15),
-//                    LocalDate.of(2022, 4, 15),
-//                    Utfall.IKKE_OPPFYLT,
-//                    ""
-//                )
-//            )
-//            .konklusjonFor(vurderingsperiode)
-//            .shouldBe(Konklusjon.Oppfylt(vurderingsperiode to vilkår.toSet()))
-//    }
+    @Test
+    fun `Teste range av ikke godkjente`() {
+        val vurderingsperiode = Periode(LocalDate.of(2022, 1, 1), LocalDate.of(2022, 1, 30))
+        val vilkår: List<Vilkår> = listOf(Vilkår.DAGPENGER, Vilkår.AAP)
+
+        vilkår
+            .map { Vurdering(it, "", null, null, Utfall.OPPFYLT, "") }
+            .plus(
+                Vurdering(
+                    Vilkår.GJENLEVENDEPENSJON,
+                    "",
+                    LocalDate.of(2022, 1, 1),
+                    LocalDate.of(2022, 1, 7),
+                    Utfall.IKKE_OPPFYLT,
+                    ""
+                )
+            ).plus(
+                Vurdering(
+                    Vilkår.LØNNSINNTEKT,
+                    "",
+                    LocalDate.of(2022, 1, 5),
+                    LocalDate.of(2022, 1, 10),
+                    Utfall.IKKE_OPPFYLT,
+                    ""
+                )
+            )
+            .plus(
+                Vurdering(
+                    Vilkår.SYKEPENGER,
+                    "",
+                    LocalDate.of(2022, 1, 9),
+                    LocalDate.of(2022, 1, 15),
+                    Utfall.IKKE_OPPFYLT,
+                    ""
+                )
+            )
+            .konklusjonFor(vurderingsperiode)
+            .shouldBe(
+                Konklusjon.DelvisOppfylt(
+                    listOf(
+                        Konklusjon.Oppfylt(
+                            Periode(LocalDate.of(2022, 1, 16), LocalDate.of(2022, 1, 30)) to vilkår.toSet()
+                        )
+                    ),
+                    listOf(
+                        Konklusjon.IkkeOppfylt(
+                            Periode(LocalDate.of(2022, 1, 1), LocalDate.of(2022, 1, 15)) to setOf(
+                                Vilkår.GJENLEVENDEPENSJON,
+                                Vilkår.LØNNSINNTEKT,
+                                Vilkår.SYKEPENGER
+                            )
+                        )
+                    )
+                )
+            )
+    }
 
     @Test
     fun `Skal få ikke-oppfylt når minst en vurdering ikke er oppfylt for hele perioden`() {
@@ -128,7 +135,7 @@ internal class KonklusjonTest {
                     Vilkår.GJENLEVENDEPENSJON,
                     "",
                     vurderingsperiode.fra,
-                    LocalDate.of(2022,1,10),
+                    LocalDate.of(2022, 1, 10),
                     Utfall.IKKE_OPPFYLT,
                     ""
                 )
@@ -137,7 +144,7 @@ internal class KonklusjonTest {
                 Vurdering(
                     Vilkår.GJENLEVENDEPENSJON,
                     "",
-                    LocalDate.of(2022,1,11),
+                    LocalDate.of(2022, 1, 11),
                     vurderingsperiode.til,
                     Utfall.IKKE_OPPFYLT,
                     ""
