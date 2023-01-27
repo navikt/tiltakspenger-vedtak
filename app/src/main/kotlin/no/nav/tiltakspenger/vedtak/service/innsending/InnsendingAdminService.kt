51fb509dd48dea99467c05616110cbbb71737e80
Hågen Hasle <haagenhasle@gmail.com>
2023-01-27 09:24:03 +0100

    Lagt på mer logging

--- app/src/main/kotlin/no/nav/tiltakspenger/vedtak/service/innsending/InnsendingAdminService.kt
+++ app/src/main/kotlin/no/nav/tiltakspenger/vedtak/service/innsending/InnsendingAdminService.kt
@@ -1,28 +1,36 @@
 package no.nav.tiltakspenger.vedtak.service.innsending
 
+import mu.KotlinLogging
 import no.nav.tiltakspenger.vedtak.Aktivitetslogg
 import no.nav.tiltakspenger.vedtak.InnsendingMediator
 import no.nav.tiltakspenger.vedtak.meldinger.ResetInnsendingHendelse
 import no.nav.tiltakspenger.vedtak.repository.InnsendingRepository
 
+private val LOG = KotlinLogging.logger {}
+
 class InnsendingAdminService(
     private val innsendingRepository: InnsendingRepository,
-    private val innsendingMediator: InnsendingMediator
+    private val innsendingMediator: InnsendingMediator,
 ) {
 
     fun resettInnsendingerSomHarFeiletEllerStoppetOpp() {
-        val innsendinger = innsendingRepository.hentInnsendingerMedTilstandFaktainnhentingFeilet() +
-            innsendingRepository.hentInnsendingerStoppetUnderBehandling()
+        val innsendinger =
+            innsendingRepository.hentInnsendingerMedTilstandFaktainnhentingFeilet()
+                .also { LOG.info { "Fant ${it.size} innsendinger i tilstand feilet" } } +
+                innsendingRepository.hentInnsendingerStoppetUnderBehandling()
+                    .also { LOG.info { "Fant ${it.size} innsendinger i tilstand stoppet eller under behandling" } }
         innsendinger.forEach {
             innsendingMediator.håndter(
                 ResetInnsendingHendelse(
                     aktivitetslogg = Aktivitetslogg(),
                     journalpostId = it,
-                )
+                ),
             )
         }
     }
 
-    fun hentInnsendingerSomErFerdigstilt() =
-        innsendingRepository.hentInnsendingerMedTilstandFerdigstilt()
+    fun hentInnsendingerSomErFerdigstilt(): List<String> =
+        innsendingRepository.hentInnsendingerMedTilstandFerdigstilt().also {
+            LOG.info { "Fant ${it.size} innsendinger i tilstand ferdigstilt" }
+        }
 }
