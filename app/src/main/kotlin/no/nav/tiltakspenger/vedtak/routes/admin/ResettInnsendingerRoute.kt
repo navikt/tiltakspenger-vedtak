b3a4083c7fb73563131e05a7b885c4797ab2af05
Steffen Lien <steffen.lien@mimedi.no>
2023-01-31 09:39:55 +0100

    legg til mulighet for å hente ut feilet og stoppet innsendinger

--- app/src/main/kotlin/no/nav/tiltakspenger/vedtak/routes/admin/ResettInnsendingerRoute.kt
+++ app/src/main/kotlin/no/nav/tiltakspenger/vedtak/routes/admin/ResettInnsendingerRoute.kt
@@ -4,14 +4,19 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import no.nav.tiltakspenger.vedtak.service.innsending.InnsendingAdminService
 
 internal const val resettFeiletOgStoppetInnsendinger = "/innsendinger/resett/feiletogstoppet"
+internal const val feiletOgStoppetInnsendinger = "/innsendinger/feiletogstoppet"
 
 fun Route.resettInnsendingerRoute(
     innsendingAdminService: InnsendingAdminService
 ) {
+    get(feiletOgStoppetInnsendinger) {
+        call.respond(innsendingAdminService.hentInnsendingerSomHarFeiletEllerStoppetOpp())
+    }
     post(resettFeiletOgStoppetInnsendinger) {
         innsendingAdminService.resettInnsendingerSomHarFeiletEllerStoppetOpp()
         call.respond(message = "Ok", status = HttpStatusCode.OK)
