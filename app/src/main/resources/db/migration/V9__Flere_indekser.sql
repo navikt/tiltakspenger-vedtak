37578c11f6e37257d8edea1e1ed240d009fac003
Hågen Hasle <haagenhasle@gmail.com>
2023-02-07 13:46:12 +0100

    Lagt til et par indekser til som manglet

---
+++ app/src/main/resources/db/migration/V9__Flere_indekser.sql
@@ -0,0 +1,11 @@
+CREATE INDEX innsending_journalpost_id
+    ON innsending
+        (
+         journalpost_id
+            );
+
+CREATE INDEX søknad_søknad_id
+    ON søknad
+        (
+         søknad_id
+            );
