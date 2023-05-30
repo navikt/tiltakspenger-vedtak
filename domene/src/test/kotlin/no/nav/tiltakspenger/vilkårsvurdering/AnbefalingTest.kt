6314a260c1cadfbae72ab7b6250ee241cd45a4c6
Hågen Hasle <haagenhasle@gmail.com>
2023-05-30 13:21:32 +0200

    Formattering

--- domene/src/test/kotlin/no/nav/tiltakspenger/vilkårsvurdering/AnbefalingTest.kt
+++ domene/src/test/kotlin/no/nav/tiltakspenger/vilkårsvurdering/AnbefalingTest.kt
@@ -127,7 +127,7 @@ internal class AnbefalingTest {
                             sykepengerVurdering,
                         ),
 
-                        ),
+                    ),
                 ),
             )
     }
