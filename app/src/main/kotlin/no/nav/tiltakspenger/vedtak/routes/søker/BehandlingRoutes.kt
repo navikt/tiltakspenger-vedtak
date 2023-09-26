f47cacf05541b8883f29d80acfb27e8fab2b1c5f
Sneha Desai <sneha.desai@nav.no>
2023-09-26 14:29:39 +0200

    La Vilkårsvurdering klass
    Co-authored-by: richardmartinsen <Richard.Martinsen@nav.no>
    Co-authored-by: benedihm <benedicte.helen.myrvoll@nav.no>
    Co-authored-by: sneha-d-desai <sneha.desai@nav.no>
    Co-authored-by: erlendev <Erlend.Edvardsen.Vige@nav.no>
    Co-authored-by: thorellefsennav <Thor.Eirik.Ellefsen@nav.no>
    Co-authored-by: KarlEvaldNav <Karl.Evald.Wigestrand@nav.no>

--- app/src/main/kotlin/no/nav/tiltakspenger/vedtak/routes/søker/BehandlingRoutes.kt
+++ app/src/main/kotlin/no/nav/tiltakspenger/vedtak/routes/søker/BehandlingRoutes.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import mu.KotlinLogging
 import no.nav.tiltakspenger.felles.BehandlingId
 import no.nav.tiltakspenger.vedtak.service.behandling.BehandlingService
@@ -14,6 +15,12 @@ private val LOG = KotlinLogging.logger {}
 
 internal const val behandlingPath = "/behandling"
 
+data class VilkårsvurderingBody(
+    val saksbehandlerId: String,
+    val vilkår: String,
+    val behandlingsId: String
+
+)
 fun Route.behandlingRoutes(
     innloggetSaksbehandlerProvider: InnloggetSaksbehandlerProvider,
     behandlingService: BehandlingService,
@@ -23,5 +30,10 @@ fun Route.behandlingRoutes(
         val behandlingId = call.parameters["behandlingId"]?.let { BehandlingId.fromDb(it) }
             ?: return@get call.respond(message = "Behandling ikke funnet", status = HttpStatusCode.NotFound)
         val behandling = behandlingService.hentBehandling(behandlingId)
+    }
+
+    post("$behandlingPath/{behandlingId}") {
+        LOG.debug("Mottatt request på $behandlingPath/")
+
     }
 }
