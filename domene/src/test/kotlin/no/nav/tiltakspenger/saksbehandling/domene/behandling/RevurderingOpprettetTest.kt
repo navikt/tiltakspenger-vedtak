caad789121c0191eb11e56e20457a51d72a33ba6
Erlend Vige <erlend.edvardsen.vige@nav.no>
2024-03-20 10:17:21 +0100

    D Skrevet tester for RevurderingOpprettetTest

---
+++ domene/src/test/kotlin/no/nav/tiltakspenger/saksbehandling/domene/behandling/RevurderingOpprettetTest.kt
@@ -0,0 +1,148 @@
+package no.nav.tiltakspenger.saksbehandling.domene.behandling
+
+import io.kotest.assertions.throwables.shouldThrowWithMessage
+import io.mockk.mockk
+import no.nav.tiltakspenger.felles.BehandlingId
+import no.nav.tiltakspenger.felles.Periode
+import no.nav.tiltakspenger.felles.Rolle
+import no.nav.tiltakspenger.felles.SakId
+import no.nav.tiltakspenger.felles.Saksbehandler
+import no.nav.tiltakspenger.saksbehandling.domene.saksopplysning.Kilde
+import no.nav.tiltakspenger.saksbehandling.domene.saksopplysning.Saksopplysning
+import no.nav.tiltakspenger.saksbehandling.domene.saksopplysning.TypeSaksopplysning
+import no.nav.tiltakspenger.saksbehandling.domene.vedtak.Vedtak
+import no.nav.tiltakspenger.saksbehandling.domene.vilkår.Vilkår
+import java.time.LocalDate
+import java.time.LocalDateTime
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+internal class RevurderingOpprettetTest {
+
+    private val saksopplysning = Saksopplysning(
+        typeSaksopplysning = TypeSaksopplysning.HAR_YTELSE,
+        fom = LocalDate.MIN,
+        tom = LocalDate.MAX,
+        vilkår = Vilkår.AAP,
+        kilde = Kilde.PESYS,
+        detaljer = "test",
+    )
+
+    private fun mockRevurderingOpprettet(
+        tiltak: List<Tiltak> = emptyList(),
+        saksbehandler: String? = null,
+    ): RevurderingOpprettet = RevurderingOpprettet(
+        id = BehandlingId.random(),
+        sakId = SakId.random(),
+        forrigeVedtak = mockk<Vedtak>(),
+        vurderingsperiode = Periode(
+            fra = LocalDate.MIN,
+            til = LocalDate.MAX,
+        ),
+        saksopplysninger = listOf(saksopplysning),
+        tiltak = tiltak,
+        saksbehandler = saksbehandler,
+        søknader = emptyList(),
+    )
+
+    private fun mockTiltak(id: String = "test"): Tiltak = Tiltak(
+        id = id,
+        kilde = "test",
+        deltakelseStatus = mockk<Tiltak.DeltakerStatus>(),
+        deltakelseFom = LocalDate.now(),
+        deltakelseTom = LocalDate.now(),
+        innhentet = LocalDateTime.now(),
+        gjennomføring = mockk<Tiltak.Gjennomføring>(),
+        registrertDato = LocalDateTime.now(),
+        deltakelseProsent = null,
+        deltakelseDagerUke = null,
+    )
+
+    private fun mockSaksbehandler(
+        navIdent: String = "test",
+        roller: List<Rolle> = emptyList(),
+    ): Saksbehandler = Saksbehandler(
+        navIdent = navIdent,
+        epost = "test",
+        brukernavn = "test",
+        roller = roller,
+    )
+
+    // TODO: For å teste motsatt tilfelle hvor det kommer inn en reelt ny saksopplysning, må vi først implementere vilkårsvurder() for Revurderingsbehandling
+    @Test
+    fun `leggTilSaksopplysning skal returnere en LeggTilSaksopplysningRespons med den samme behandlingen dersom saksopplysningene ikke har endret seg fra sist`() {
+        val revurderingOpprettet = mockRevurderingOpprettet()
+        val leggTilSaksopplysningRespons = revurderingOpprettet.leggTilSaksopplysning(saksopplysning)
+        assertEquals(revurderingOpprettet, leggTilSaksopplysningRespons.behandling)
+        assertFalse { leggTilSaksopplysningRespons.erEndret }
+    }
+
+    @Test
+    fun `oppdaterTiltak skal returnere en kopi av behandlingen med de nye tiltakene lagt inn`() {
+        val gamleTiltak = listOf(mockTiltak())
+        val nyeTiltak = listOf(mockTiltak(id = "nyttTiltak"))
+        val revurderingOpprettetMedGamleTiltak = mockRevurderingOpprettet(tiltak = gamleTiltak)
+        val revurderingOppprettetMedNyeTiltak = revurderingOpprettetMedGamleTiltak.oppdaterTiltak(nyeTiltak)
+        assertTrue { revurderingOpprettetMedGamleTiltak.tiltak == gamleTiltak }
+        assertTrue { revurderingOppprettetMedNyeTiltak.tiltak == nyeTiltak }
+    }
+
+    @Test
+    fun `startBehandling skal kaste exception med feilmelding hvis man prøver å starte en behandling som allerede er tatt`() {
+        val revurderingOpprettet = mockRevurderingOpprettet(saksbehandler = "test")
+        shouldThrowWithMessage<IllegalStateException>("Denne behandlingen er allerede tatt") {
+            revurderingOpprettet.startBehandling(saksbehandler = mockSaksbehandler())
+        }
+    }
+
+    @Test
+    fun `startBehandling skal kaste exception med feilmelding hvis man prøver å starte en behandling uten at man har rollen som saksbehandler`() {
+        val revurderingOpprettet = mockRevurderingOpprettet()
+        shouldThrowWithMessage<IllegalStateException>("Saksbehandler må være saksbehandler") {
+            revurderingOpprettet.startBehandling(saksbehandler = mockSaksbehandler())
+        }
+    }
+
+    @Test
+    fun `startBehandling skal returnere en kopi av behandlingen med saksbehandler-felt satt hvis det går ok å starte behandlingen`() {
+        val revurderingOpprettet = mockRevurderingOpprettet()
+        val igangsattRevurderingsbehandling = revurderingOpprettet.startBehandling(
+            saksbehandler = mockSaksbehandler(
+                navIdent = "saksbehandler",
+                roller = listOf(Rolle.SAKSBEHANDLER),
+            ),
+        )
+        assertNull(revurderingOpprettet.saksbehandler)
+        assertEquals(igangsattRevurderingsbehandling.saksbehandler, "saksbehandler")
+    }
+
+    @Test
+    fun `avbrytBehandling skal kaste exception med feilmelding hvis man prøver å avbryte en behandling uten at man er saksbehandler eller admin`() {
+        val revurderingOpprettet = mockRevurderingOpprettet()
+        val saksbehandler = mockSaksbehandler(navIdent = "test")
+        shouldThrowWithMessage<IllegalStateException>("Kan ikke avbryte en behandling som ikke er din") {
+            revurderingOpprettet.avbrytBehandling(saksbehandler)
+        }
+    }
+
+    @Test
+    fun `avbrytBehandling skal returnere med en kopi av behandlingen, med saksbehandler satt til null, hvis man prøver å avbryte behandlingen som saksbehandler`() {
+        val revurderingOpprettet = mockRevurderingOpprettet(saksbehandler = "test")
+        val saksbehandler = mockSaksbehandler(navIdent = "test", roller = listOf(Rolle.SAKSBEHANDLER))
+        val avbruttBehandling = revurderingOpprettet.avbrytBehandling(saksbehandler)
+        assertEquals(revurderingOpprettet.saksbehandler, "test")
+        assertNull(avbruttBehandling.saksbehandler)
+    }
+
+    @Test
+    fun `avbrytBehandling skal returnere med en kopi av behandlingen, med saksbehandler satt til null, hvis man prøver å avbryte behandlingen som admin`() {
+        val revurderingOpprettet = mockRevurderingOpprettet(saksbehandler = "test")
+        val saksbehandler = mockSaksbehandler(navIdent = "test", roller = listOf(Rolle.ADMINISTRATOR))
+        val avbruttBehandling = revurderingOpprettet.avbrytBehandling(saksbehandler)
+        assertEquals(revurderingOpprettet.saksbehandler, "test")
+        assertNull(avbruttBehandling.saksbehandler)
+    }
+}
