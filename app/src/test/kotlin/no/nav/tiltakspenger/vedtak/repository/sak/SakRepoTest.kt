dd6f058e355435bcdcf123fbe635e389f5a45736
Erlend Vige <erlend.edvardsen.vige@nav.no>
2024-03-25 09:15:32 +0100

    La til to tester i SakRepoTest
    * Test av hentForSaksnummer
    * Test av hentForIdent

--- app/src/test/kotlin/no/nav/tiltakspenger/vedtak/repository/sak/SakRepoTest.kt
+++ app/src/test/kotlin/no/nav/tiltakspenger/vedtak/repository/sak/SakRepoTest.kt
@@ -1,5 +1,8 @@
 package no.nav.tiltakspenger.vedtak.repository.sak
 
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldNotContain
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import no.nav.tiltakspenger.felles.Periode
 import no.nav.tiltakspenger.felles.SakId
@@ -15,6 +18,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
+import java.time.LocalDate
 import java.util.Random
 
 @Testcontainers
@@ -31,26 +35,56 @@ internal class SakRepoTest {
         flywayCleanAndMigrate()
     }
 
+    private fun mockSak(
+        ident: String = Random().nextInt().toString(),
+        startdato: LocalDate = 1.januar(2023),
+        sluttdato: LocalDate = 31.mars(2023),
+        saksnummer: Saksnummer = Saksnummer(verdi = "123"),
+    ): Sak = Sak(
+        id = SakId.random(),
+        ident = ident,
+        saknummer = saksnummer,
+        periode = Periode(fra = startdato, til = sluttdato),
+        behandlinger = listOf(),
+        personopplysninger = SakPersonopplysninger(),
+        vedtak = listOf(),
+    )
+
     @Test
     fun `lagre og hente en sak med en søknad`() {
         val ident = Random().nextInt().toString()
-        val startDato = 1.januar(2023)
+        val startdato = 1.januar(2023)
         val sluttdato = 31.mars(2023)
 
-        val sak = Sak(
-            id = SakId.random(),
-            ident = ident,
-            saknummer = Saksnummer(verdi = "123"),
-            periode = Periode(fra = startDato, til = sluttdato),
-            behandlinger = listOf(),
-            personopplysninger = SakPersonopplysninger(),
-            vedtak = listOf(),
-        )
+        val sak = mockSak(ident = ident, startdato = startdato, sluttdato = sluttdato)
 
         sakRepo.lagre(sak)
 
-        val hentSak = sakRepo.hentForIdentMedPeriode(ident, Periode(fra = startDato, til = sluttdato))
+        val hentSak = sakRepo.hentForIdentMedPeriode(ident, Periode(fra = startdato, til = sluttdato))
 
         hentSak shouldNotBe emptyList<Sak>()
+    }
+
+    @Test
+    fun `hentForSaksnummer skal hente sak med matchende saksnummer`() {
+        val saksnummerMock = Saksnummer(verdi = "123456789")
+        val sak = mockSak(saksnummer = saksnummerMock)
+        sakRepo.lagre(sak)
+        val sakHentetUtifraSaksnummer = sakRepo.hentForSaksnummer(saksnummer = saksnummerMock.verdi)
+        sakHentetUtifraSaksnummer?.saknummer shouldBe sak.saknummer
+    }
+
+    @Test
+    fun `hentForIdent skal hente saker med matchende ident`() {
+        val ident = "123"
+        val sak1 = mockSak(ident = ident).also { sakRepo.lagre(it) }
+        val sak2 = mockSak(ident = ident).also { sakRepo.lagre(it) }
+        val sak3 = mockSak(ident = "456").also { sakRepo.lagre(it) }
+
+        val sakerMedIdent = sakRepo.hentForIdent(ident)
+        sakerMedIdent.size shouldBe 2
+        sakerMedIdent shouldContain sak1
+        sakerMedIdent shouldContain sak2
+        sakerMedIdent shouldNotContain sak3
     }
 }
