674a25754d6cc22461dceb335adef00391cb1a3a
Erlend Vige <erlend.edvardsen.vige@nav.no>
2024-03-25 09:35:06 +0100

    Refaktorering SakRepoTest
    Oppdaterer SakRepoTest til å bruke tomSak fra SakMother til å mocke ut
    Sak, istedetfor å lage en egen mockSak-funksjon

--- app/src/test/kotlin/no/nav/tiltakspenger/vedtak/repository/sak/SakRepoTest.kt
+++ app/src/test/kotlin/no/nav/tiltakspenger/vedtak/repository/sak/SakRepoTest.kt
@@ -5,10 +5,9 @@ import io.kotest.matchers.collections.shouldNotContain
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import no.nav.tiltakspenger.felles.Periode
-import no.nav.tiltakspenger.felles.SakId
 import no.nav.tiltakspenger.felles.januar
 import no.nav.tiltakspenger.felles.mars
-import no.nav.tiltakspenger.saksbehandling.domene.personopplysninger.SakPersonopplysninger
+import no.nav.tiltakspenger.objectmothers.ObjectMother.tomSak
 import no.nav.tiltakspenger.saksbehandling.domene.sak.Sak
 import no.nav.tiltakspenger.saksbehandling.domene.sak.Saksnummer
 import no.nav.tiltakspenger.saksbehandling.ports.SakRepo
@@ -18,7 +17,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-import java.time.LocalDate
 import java.util.Random
 
 @Testcontainers
@@ -35,28 +33,13 @@ internal class SakRepoTest {
         flywayCleanAndMigrate()
     }
 
-    private fun mockSak(
-        ident: String = Random().nextInt().toString(),
-        startdato: LocalDate = 1.januar(2023),
-        sluttdato: LocalDate = 31.mars(2023),
-        saksnummer: Saksnummer = Saksnummer(verdi = "123"),
-    ): Sak = Sak(
-        id = SakId.random(),
-        ident = ident,
-        saknummer = saksnummer,
-        periode = Periode(fra = startdato, til = sluttdato),
-        behandlinger = listOf(),
-        personopplysninger = SakPersonopplysninger(),
-        vedtak = listOf(),
-    )
-
     @Test
     fun `lagre og hente en sak med en søknad`() {
         val ident = Random().nextInt().toString()
         val startdato = 1.januar(2023)
         val sluttdato = 31.mars(2023)
 
-        val sak = mockSak(ident = ident, startdato = startdato, sluttdato = sluttdato)
+        val sak = tomSak(ident = ident, periode = Periode(fra = startdato, til = sluttdato))
 
         sakRepo.lagre(sak)
 
@@ -68,7 +51,7 @@ internal class SakRepoTest {
     @Test
     fun `hentForSaksnummer skal hente sak med matchende saksnummer`() {
         val saksnummerMock = Saksnummer(verdi = "123456789")
-        val sak = mockSak(saksnummer = saksnummerMock)
+        val sak = tomSak(saksnummer = saksnummerMock)
         sakRepo.lagre(sak)
         val sakHentetUtifraSaksnummer = sakRepo.hentForSaksnummer(saksnummer = saksnummerMock.verdi)
         sakHentetUtifraSaksnummer?.saknummer shouldBe sak.saknummer
@@ -77,9 +60,9 @@ internal class SakRepoTest {
     @Test
     fun `hentForIdent skal hente saker med matchende ident`() {
         val ident = "123"
-        val sak1 = mockSak(ident = ident).also { sakRepo.lagre(it) }
-        val sak2 = mockSak(ident = ident).also { sakRepo.lagre(it) }
-        val sak3 = mockSak(ident = "456").also { sakRepo.lagre(it) }
+        val sak1 = tomSak(ident = ident).also { sakRepo.lagre(it) }
+        val sak2 = tomSak(ident = ident).also { sakRepo.lagre(it) }
+        val sak3 = tomSak(ident = "456").also { sakRepo.lagre(it) }
 
         val sakerMedIdent = sakRepo.hentForIdent(ident)
         sakerMedIdent.size shouldBe 2
