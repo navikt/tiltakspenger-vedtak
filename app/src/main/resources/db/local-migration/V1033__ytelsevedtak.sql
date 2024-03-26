51787952f88afeca995f8476fbb3a977b72e0646
Thor Eirik Ellefsen <thor.eirik.ellefsen@nav.no>
2024-03-26 15:23:18 +0100

    F - Oppdater lokale testdata

--- app/src/main/resources/db/local-migration/V1033__ytelsevedtak.sql
+++ app/src/main/resources/db/local-migration/V1033__ytelsevedtak.sql
@@ -1,5 +1,7 @@
-INSERT INTO public.ytelsevedtak (id, ytelsesak_id, beslutnings_dato, periodetype_for_ytelse, vedtaksperiode_fom, vedtaksperiode_tom, vedtaks_type, status) VALUES ('aved_01HGFZTVGRR6SW0GF5C4Y8152V', 'ayt_01HGFZTVGPTNZR1G2ZKK111CR4', '2023-11-24', 'O', '2023-10-01', '2025-03-28', 'BASI', 'IVERK');
-INSERT INTO public.ytelsevedtak (id, ytelsesak_id, beslutnings_dato, periodetype_for_ytelse, vedtaksperiode_fom, vedtaksperiode_tom, vedtaks_type, status) VALUES ('aved_01HHBY69JXEH4A10ZD0G4D0B96', 'ayt_01HHBY69JWE7020CDJ8XXA0XEE', '2023-11-24', 'O', '2023-10-01', '2034-02-28', 'BASI', 'IVERK');
-INSERT INTO public.ytelsevedtak (id, ytelsesak_id, beslutnings_dato, periodetype_for_ytelse, vedtaksperiode_fom, vedtaksperiode_tom, vedtaks_type, status) VALUES ('aved_01HHBYF1DZMA7214BM5CEQ0CBN', 'ayt_01HHBYF1DYHPY80MQAFK4M000J', '2023-11-24', 'O', '2023-10-01', '2034-02-28', 'BASI', 'IVERK');
-INSERT INTO public.ytelsevedtak (id, ytelsesak_id, beslutnings_dato, periodetype_for_ytelse, vedtaksperiode_fom, vedtaksperiode_tom, vedtaks_type, status) VALUES ('aved_01HHHTNVPH5PF01PMWXQDR1WH7', 'ayt_01HHHTNVPG0S580JG9Z2HC058F', '2023-11-24', 'O', '2023-10-01', '2034-02-28', 'BASI', 'IVERK');
-INSERT INTO public.ytelsevedtak (id, ytelsesak_id, beslutnings_dato, periodetype_for_ytelse, vedtaksperiode_fom, vedtaksperiode_tom, vedtaks_type, status) VALUES ('aved_01HHYEAKFA4FNQ1JDY3EMS1F6S', 'ayt_01HHYEAKF9PAY31QJC59Y00ZHX', '2021-08-17', 'O', '2021-08-17', '2024-06-30', 'BASI', 'IVERK');
+INSERT INTO public.ytelsevedtak (id,ytelsesak_id,beslutnings_dato,periodetype_for_ytelse,vedtaksperiode_fom,vedtaksperiode_tom,vedtaks_type,status) VALUES
+    ('aved_01HSGH6K5PPZBGJRHTHK2G2FSR','ayt_01HSGH6K5N0FRE6P1EH6SMMWCY','2024-01-30','O','2023-01-01','2024-06-30','BASI','IVERK'),
+    ('aved_01HSGH6K5RKK8BRXW29WMY3WYA','ayt_01HSGH6K5N0FRE6P1EH6SMMWCY','2024-01-30','O','2023-01-01','2024-06-30','BTIL','IVERK'),
+    ('aved_01HSJYW112A4KXBTSZ77QQ5B87','ayt_01HSJYW111WJNA8J1F65EV3BTS','2024-01-30','O','2023-01-01','2024-06-30','BASI','IVERK'),
+    ('aved_01HSJYW113BXN74X9G14FDY1Y4','ayt_01HSJYW111WJNA8J1F65EV3BTS','2024-01-30','O','2023-01-01','2024-06-30','BTIL','IVERK'),
+    ('aved_01HSK4S2JQ2C4QS1FMXDKK7KS1','ayt_01HSK4S2JNJAW3KN5XKZZN7JJV','2024-01-10','E','2024-01-01','2024-05-01','BASI','IVERK'),
+    ('aved_01HSK4S2JRWVTHV7N009Q5GNXG','ayt_01HSK4S2JNJAW3KN5XKZZN7JJV','2023-11-24','O','2023-11-01','2023-12-31','BASI','AVSLU');
