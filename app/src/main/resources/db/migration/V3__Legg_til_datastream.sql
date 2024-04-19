9e8cbda653e20bba1837618112ce5cb2e20bc8e9
Richard Martinsen <github@martinsen.org>
2024-04-19 13:29:36 +0200

    F - Legg til datastream
    Co-authored-by: thorellefsennav <Thor.Eirik.Ellefsen@nav.no>

--- app/src/main/resources/db/migration/V3__Legg_til_datastream.sql
+++ app/src/main/resources/db/migration/V3__Legg_til_datastream.sql
@@ -1,7 +1,16 @@
-ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT ON TABLES TO "datastream";
-GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "datastream";
+DO
+$$
+    BEGIN
+        IF
+            EXISTS
+                (SELECT 1 from pg_roles where rolname = 'tpts_datastream')
+        THEN
+            ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT ON TABLES TO "tpts_datastream";
+            GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "tpts_datastream";
 
-ALTER USER "tiltakspenger-vedtak" WITH REPLICATION;
-ALTER USER "tpts_datastream" WITH REPLICATION;
-CREATE PUBLICATION "ds_publication" FOR ALL TABLES;
---SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+            ALTER USER "tiltakspenger-vedtak" WITH REPLICATION;
+            ALTER USER "tpts_datastream" WITH REPLICATION;
+            CREATE PUBLICATION "ds_publication" FOR ALL TABLES;
+        END IF;
+    END
+$$;
