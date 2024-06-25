42a12742980589494a81d3169ce732662bec7422
karlevaldnav <karl.evald.wigestrand@nav.no>
2024-06-25 11:31:22 +0200

    R Legg til try-catch rundt parsing av json så man ser hva som feiler + stacktrace i loggen

--- app/src/main/kotlin/no/nav/tiltakspenger/vedtak/repository/behandling/kvp/VilkårsettDbJson.kt
+++ app/src/main/kotlin/no/nav/tiltakspenger/vedtak/repository/behandling/kvp/VilkårsettDbJson.kt
@@ -6,6 +6,7 @@ import no.nav.tiltakspenger.saksbehandling.domene.vilkår.Vilkårssett
 import no.nav.tiltakspenger.saksbehandling.domene.vilkår.Vurdering
 import no.nav.tiltakspenger.vedtak.db.deserialize
 import no.nav.tiltakspenger.vedtak.db.serialize
+import java.security.InvalidParameterException
 
 /**
  * Har ansvar for å serialisere/deserialisere Vilkårssett til og fra json for lagring i database.
@@ -19,13 +20,17 @@ internal fun String.toVilkårssett(
     vilkårsvurderinger: List<Vurdering>,
     kravdatoSaksopplysninger: KravdatoSaksopplysninger,
 ): Vilkårssett {
-    val vilkårssettJson = deserialize<VilkårssettJson>(this)
-    return Vilkårssett(
-        kvpVilkår = vilkårssettJson.kvpVilkår.toDomain(),
-        saksopplysninger = saksopplysninger,
-        vilkårsvurderinger = vilkårsvurderinger,
-        kravdatoSaksopplysninger = kravdatoSaksopplysninger,
-    )
+    try {
+        val vilkårssettJson = deserialize<VilkårssettJson>(this)
+        return Vilkårssett(
+            kvpVilkår = vilkårssettJson.kvpVilkår.toDomain(),
+            saksopplysninger = saksopplysninger,
+            vilkårsvurderinger = vilkårsvurderinger,
+            kravdatoSaksopplysninger = kravdatoSaksopplysninger,
+        )
+    } catch (exception: Exception) {
+        throw InvalidParameterException("Det oppstod en feil ved parsing av json: " + exception.message)
+    }
 }
 
 internal fun Vilkårssett.toDbJson(): String {
