fea20de9597de453b3d83b1258e48800e7c36b99
John Andre Hestad <john.andre.hestad@nav.no>
2024-07-04 16:59:30 +0200

    Legg ved mer context når synkront kall mot PDL feiler

--- app/src/main/kotlin/no/nav/tiltakspenger/vedtak/clients/person/PersonHttpklient.kt
+++ app/src/main/kotlin/no/nav/tiltakspenger/vedtak/clients/person/PersonHttpklient.kt
@@ -1,6 +1,16 @@
 package no.nav.tiltakspenger.vedtak.clients.person
 
 import no.nav.tiltakspenger.libs.personklient.pdl.FellesPersonklient
+import no.nav.tiltakspenger.libs.personklient.pdl.FellesPersonklientError.AdressebeskyttelseKunneIkkeAvklares
+import no.nav.tiltakspenger.libs.personklient.pdl.FellesPersonklientError.DeserializationException
+import no.nav.tiltakspenger.libs.personklient.pdl.FellesPersonklientError.FantIkkePerson
+import no.nav.tiltakspenger.libs.personklient.pdl.FellesPersonklientError.FødselKunneIkkeAvklares
+import no.nav.tiltakspenger.libs.personklient.pdl.FellesPersonklientError.Ikke2xx
+import no.nav.tiltakspenger.libs.personklient.pdl.FellesPersonklientError.IngenNavnFunnet
+import no.nav.tiltakspenger.libs.personklient.pdl.FellesPersonklientError.NavnKunneIkkeAvklares
+import no.nav.tiltakspenger.libs.personklient.pdl.FellesPersonklientError.NetworkError
+import no.nav.tiltakspenger.libs.personklient.pdl.FellesPersonklientError.ResponsManglerPerson
+import no.nav.tiltakspenger.libs.personklient.pdl.FellesPersonklientError.UkjentFeil
 import no.nav.tiltakspenger.saksbehandling.domene.personopplysninger.Personopplysninger
 import no.nav.tiltakspenger.saksbehandling.ports.PersonGateway
 import no.nav.tiltakspenger.vedtak.auth.AzureTokenProvider
@@ -22,7 +32,28 @@ internal class PersonHttpklient(
         val token = azureTokenProvider.getToken()
         return personklient.hentPerson(ident, token).fold(
             // TODO jah: Her har vi mulighet til å returnere Either.left istedet for å kaste.
-            ifLeft = { throw RuntimeException("Feil ved henting av personopplysninger for ident $ident") },
+            ifLeft = {
+                when (it) {
+                    is AdressebeskyttelseKunneIkkeAvklares -> throw RuntimeException("Feil ved henting av personopplysninger: AdressebeskyttelseKunneIkkeAvklares")
+                    is DeserializationException -> throw RuntimeException(
+                        "Feil ved henting av personopplysninger: DeserializationException",
+                        it.exception,
+                    )
+
+                    is FantIkkePerson -> throw RuntimeException("Feil ved henting av personopplysninger: FantIkkePerson")
+                    is FødselKunneIkkeAvklares -> throw RuntimeException("Feil ved henting av personopplysninger: FødselKunneIkkeAvklares")
+                    is Ikke2xx -> throw RuntimeException("Feil ved henting av personopplysninger: $it")
+                    is IngenNavnFunnet -> throw RuntimeException("Feil ved henting av personopplysninger: IngenNavnFunnet")
+                    is NavnKunneIkkeAvklares -> throw RuntimeException("Feil ved henting av personopplysninger: NavnKunneIkkeAvklares")
+                    is NetworkError -> throw RuntimeException(
+                        "Feil ved henting av personopplysninger: NetworkError",
+                        it.exception,
+                    )
+
+                    is ResponsManglerPerson -> throw RuntimeException("Feil ved henting av personopplysninger: ResponsManglerPerson")
+                    is UkjentFeil -> throw RuntimeException("Feil ved henting av personopplysninger: $it")
+                }
+            },
             ifRight = { (person, _) -> mapPersonopplysninger(person, LocalDateTime.now(), ident) },
         )
     }
