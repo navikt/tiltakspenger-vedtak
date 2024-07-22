a53119c57f0be01e6c544653b92c447a92becb8b
Benedicte Myrvoll <benemyr17@gmail.com>
2024-07-22 14:04:54 +0200

    F Ta inn nye vilkår inn i VilkårssettDTO

--- app/src/main/kotlin/no/nav/tiltakspenger/vedtak/routes/behandling/vilkår/VilkårsettDTO.kt
+++ app/src/main/kotlin/no/nav/tiltakspenger/vedtak/routes/behandling/vilkår/VilkårsettDTO.kt
@@ -1,19 +1,31 @@
 package no.nav.tiltakspenger.vedtak.routes.behandling.vilkår
 
 import no.nav.tiltakspenger.saksbehandling.domene.vilkår.Vilkårssett
+import no.nav.tiltakspenger.vedtak.routes.behandling.vilkår.institusjonsopphold.InstitusjonsoppholdVilkårDTO
+import no.nav.tiltakspenger.vedtak.routes.behandling.vilkår.institusjonsopphold.toDTO
+import no.nav.tiltakspenger.vedtak.routes.behandling.vilkår.introduksjonsprogrammet.IntroVilkårDTO
+import no.nav.tiltakspenger.vedtak.routes.behandling.vilkår.introduksjonsprogrammet.toDTO
 import no.nav.tiltakspenger.vedtak.routes.behandling.vilkår.kvp.KVPVilkårDTO
 import no.nav.tiltakspenger.vedtak.routes.behandling.vilkår.kvp.toDTO
+import no.nav.tiltakspenger.vedtak.routes.behandling.vilkår.livsopphold.LivsoppholdVilkårDTO
+import no.nav.tiltakspenger.vedtak.routes.behandling.vilkår.livsopphold.toDTO
 import no.nav.tiltakspenger.vedtak.routes.dto.PeriodeDTO
 
 /**
  * Har ansvar for å serialisere Vilkårssett til json. Kontrakt mot frontend.
  */
-internal class VilkårssettDTO(
+internal data class VilkårssettDTO(
     val kvpVilkår: KVPVilkårDTO,
+    val introVilkår: IntroVilkårDTO,
+    val institusjonsoppholdVilkår: InstitusjonsoppholdVilkårDTO,
+    val livsoppholdVilkår: LivsoppholdVilkårDTO,
 )
 
 internal fun Vilkårssett.toDTO(vurderingsperiode: PeriodeDTO): VilkårssettDTO {
     return VilkårssettDTO(
         kvpVilkår = kvpVilkår.toDTO(vurderingsperiode),
+        introVilkår = introVilkår.toDTO(vurderingsperiode),
+        institusjonsoppholdVilkår = institusjonsoppholdVilkår.toDTO(vurderingsperiode),
+        livsoppholdVilkår = livsoppholdVilkår.toDTO(),
     )
 }
