4f9093f95702a4fe1349c1a8a87486e3d5a0e5dc
Richard Martinsen <github@martinsen.org>
2024-08-22 11:46:24 +0200

    F - Legg til en sak og behandling i lokal migrering

--- app/src/main/resources/db/local-migration/V1006__vedtak.sql
+++ app/src/main/resources/db/local-migration/V1006__vedtak.sql
@@ -1,2 +1,4 @@
 INSERT INTO public.vedtak (id,sak_id,behandling_id,vedtakstype,vedtaksdato,fom,tom,saksbehandler,beslutter,opprettet) VALUES
     ('vedtak_01HSJYYM22NG6X187YK5NNCJ60','sak_01HSJYVBXR7YSXPZBGF2K2VCRW','beh_01HSJYVBXRP027W70B8RE38NHH','INNVILGELSE','2024-03-22 12:32:46.274822+01','2024-01-01','2024-06-30','Z123456','Z994363','2024-03-22 12:32:46.341938+01');
+INSERT INTO public.vedtak (id, sak_id, behandling_id, vedtakstype, vedtaksdato, fom, tom, saksbehandler, beslutter, opprettet, sendt_til_meldekort) VALUES
+    ('vedtak_01J5WNV7AT1V53Z6GPEGFRRPEJ', 'sak_01J5WNP8W7FNHEK8AVPBVJXQ26', 'beh_01J5WNP8W8AX9VC324PT633SY6', 'INNVILGELSE', '2024-08-22 09:14:23.962631 +00:00', '2024-08-01', '2024-12-31', 'Z994128', 'Z994127', '2024-08-22 09:14:23.972693 +00:00', true);
