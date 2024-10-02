aa17fb5c428ab9fc1dc07a0663857800bbef7f3c
Benedicte Myrvoll <benemyr17@gmail.com>
2024-10-02 09:03:42 +0200

    R Fjern TilgangAdressebeskyttelseProvider.kt

--- app/src/main/kotlin/no/nav/tiltakspenger/vedtak/tilgang/TilgangAdressebeskyttelseProvider.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.tiltakspenger.vedtak.tilgang
-
-import no.nav.tiltakspenger.libs.common.Fnr
-
-interface TilgangAdressebeskyttelseProvider {
-    suspend fun sjekkTilgangEnkel(fnr: Fnr)
-
-    suspend fun sjekkTilgangBolk(fnrListe: List<Fnr>)
-}
