4bd9cc09f6a0281eacf468959f690eaf74b27a6e
Anders B Nomerstad <anders@nomerstad.com>
2024-12-10 12:38:18 +0100

    Oppdaterer endepunkt for sending til meldekort-api. Fikser lokalt azure env

--- app/src/main/kotlin/no/nav/tiltakspenger/vedtak/clients/meldekort/MeldekortApiHttpClient.kt
+++ app/src/main/kotlin/no/nav/tiltakspenger/vedtak/clients/meldekort/MeldekortApiHttpClient.kt
@@ -27,7 +27,7 @@ class MeldekortApiHttpClient(
         return Either.catch {
             val body = meldekort.tilUtfyllingDTO()
             val token = entraIdSystemtokenClient.getSystemtoken(scope)
-            val response = client.preparePost("$baseUrl/meldekort/til-utfylling") {
+            val response = client.preparePost("$baseUrl/saksbehandling/meldekort") {
                 bearerAuth(token.token)
                 contentType(ContentType.Application.Json)
                 setBody(body)
