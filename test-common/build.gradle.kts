b7e6c34ea964abc0ac51c37bedf5cf6654690b1d
Anders B Nomerstad <anders@nomerstad.com>
2024-12-16 13:27:14 +0100

    Revert fellesLib oppdatering (302 -> 294). Kjører tester før dependabot PR auto-merge.

--- test-common/build.gradle.kts
+++ test-common/build.gradle.kts
@@ -1,6 +1,6 @@
 val kotlinxCoroutinesVersion = "1.9.0"
 val kotestVersion = "5.9.1"
-val felleslibVersion = "0.0.302"
+val felleslibVersion = "0.0.294"
 
 dependencies {
     api(project(":domene"))
