9612448ebd4753f053c38b88631d48d799bf6002
Anders B Nomerstad <anders@nomerstad.com>
2024-12-17 10:00:10 +0100

    Reenable sending til meldekort-api

--- domene/src/main/kotlin/no/nav/tiltakspenger/meldekort/service/SendMeldekortTilBrukerService.kt
+++ domene/src/main/kotlin/no/nav/tiltakspenger/meldekort/service/SendMeldekortTilBrukerService.kt
@@ -2,6 +2,8 @@ package no.nav.tiltakspenger.meldekort.service
 
 import arrow.core.Either
 import mu.KotlinLogging
+import no.nav.tiltakspenger.felles.sikkerlogg
+import no.nav.tiltakspenger.libs.common.nå
 import no.nav.tiltakspenger.meldekort.ports.MeldekortApiHttpClientGateway
 import no.nav.tiltakspenger.meldekort.ports.MeldekortRepo
 
@@ -16,20 +18,23 @@ class SendMeldekortTilBrukerService(
 
     suspend fun send() {
         Either.catch {
-//            val meldekortTilBruker = meldekortRepo.hentUsendteTilBruker()
-//
-//            logger.info("Fant ${meldekortTilBruker.count()} meldekort for sending til meldekort-api")
-//
-//            meldekortTilBruker.forEach { meldekort ->
-//                meldekortApiHttpClient.sendMeldekort(meldekort).onRight {
-//                    logger.info { "Sendte meldekort til meldekort-api med id ${meldekort.id}" }
-//                    meldekortRepo.markerSomSendtTilBruker(meldekort.id, nå())
-//                }.onLeft {
-//                    logger.error { "Kunne ikke sende meldekort til utfylling med id ${meldekort.id}" }
-//                }
-//            }
+            val meldekortTilBruker = meldekortRepo.hentUsendteTilBruker()
+
+            logger.debug("Fant ${meldekortTilBruker.count()} meldekort for sending til meldekort-api")
+
+            meldekortTilBruker.forEach { meldekort ->
+                meldekortApiHttpClient.sendMeldekort(meldekort).onRight {
+                    logger.info { "Sendte meldekort til meldekort-api med id ${meldekort.id}" }
+                    meldekortRepo.markerSomSendtTilBruker(meldekort.id, nå())
+                }.onLeft {
+                    logger.error { "Kunne ikke sende meldekort til utfylling med id ${meldekort.id}" }
+                }
+            }
         }.onLeft {
-            logger.error(RuntimeException("Oh noes")) { "Feil ved sending av meldekort til meldekort-api! ${it.message}" }
+            "Feil ved sending av meldekort til meldekort-api!".let { msg ->
+                logger.error(RuntimeException("Uventet feil!")) { msg }
+                sikkerlogg.error(it) { msg }
+            }
         }
     }
 }
