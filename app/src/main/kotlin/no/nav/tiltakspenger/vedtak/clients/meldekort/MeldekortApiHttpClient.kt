ea8a1f98357b5f6d5d2b6f18d6227b640cb681bb
Anders B Nomerstad <anders@nomerstad.com>
2024-12-17 15:16:04 +0100

    Bytter ut ktor client med innebygd

--- app/src/main/kotlin/no/nav/tiltakspenger/vedtak/clients/meldekort/MeldekortApiHttpClient.kt
+++ app/src/main/kotlin/no/nav/tiltakspenger/vedtak/clients/meldekort/MeldekortApiHttpClient.kt
@@ -2,40 +2,43 @@ package no.nav.tiltakspenger.vedtak.clients.meldekort
 
 import arrow.core.Either
 import arrow.core.left
-import io.ktor.client.call.body
-import io.ktor.client.request.bearerAuth
-import io.ktor.client.request.preparePost
-import io.ktor.client.request.setBody
-import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode
-import io.ktor.http.contentType
+import kotlinx.coroutines.future.await
 import mu.KotlinLogging
 import no.nav.tiltakspenger.felles.sikkerlogg
 import no.nav.tiltakspenger.libs.common.AccessToken
+import no.nav.tiltakspenger.libs.json.serialize
 import no.nav.tiltakspenger.meldekort.domene.Meldekort
 import no.nav.tiltakspenger.meldekort.ports.FeilVedSendingTilMeldekortApi
 import no.nav.tiltakspenger.meldekort.ports.MeldekortApiHttpClientGateway
-import no.nav.tiltakspenger.vedtak.clients.defaultHttpClient
+import java.net.URI
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 class MeldekortApiHttpClient(
     baseUrl: String,
     private val getToken: suspend () -> AccessToken,
 ) : MeldekortApiHttpClientGateway {
-    private val client = defaultHttpClient()
+    private val client = java.net.http.HttpClient
+        .newBuilder()
+        .followRedirects(java.net.http.HttpClient.Redirect.NEVER)
+        .build()
+
     private val logger = KotlinLogging.logger {}
-    private val url = "$baseUrl/meldekort"
+
+    private val meldekortApiUri = URI.create("$baseUrl/meldekort")
 
     override suspend fun sendMeldekort(meldekort: Meldekort): Either<FeilVedSendingTilMeldekortApi, Unit> {
         return Either.catch {
-            val response = client.preparePost(url) {
-                bearerAuth(getToken().token)
-                contentType(ContentType.Application.Json)
-                setBody(meldekort.tilBrukerDTO())
-            }.execute()
+            val response = client.sendAsync(
+                createRequest(meldekort),
+                HttpResponse.BodyHandlers.ofString(),
+            ).await()
 
-            if (response.status != HttpStatusCode.OK) {
-                val body: String = response.call.body()
-                "Feilrespons ved sending av ${meldekort.id} til meldekort-api - status: ${response.status}".let { msg ->
+            val status = response.statusCode()
+
+            if (status !in 200..299) {
+                val body: String = response.body()
+                "Feilrespons ved sending av ${meldekort.id} til meldekort-api - status: $status".let { msg ->
                     logger.error(msg)
                     sikkerlogg.error { "$msg - Response body: $body" }
                 }
@@ -48,5 +51,19 @@ class MeldekortApiHttpClient(
             }
             FeilVedSendingTilMeldekortApi
         }
+    }
+
+    private suspend fun createRequest(
+        meldekort: Meldekort,
+    ): HttpRequest {
+        val payload = serialize(meldekort.tilBrukerDTO())
+
+        return HttpRequest
+            .newBuilder()
+            .uri(meldekortApiUri)
+            .header("Authorization", "Bearer ${getToken().token}")
+            .header("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(payload))
+            .build()
     }
 }
